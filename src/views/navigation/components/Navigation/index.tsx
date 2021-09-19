1a6eab9fe7b0635281069dc3e302293336d63d75
Mashustia <denisova.mariya.dm@gmail.com>
2021-09-20 00:51:54 +0300

    feat(navigation): add navigation component

---
+++ src/views/navigation/components/Navigation/index.tsx
@@ -0,0 +1,13 @@
+import React, {Component, ReactElement} from 'react'
+
+// const styles = require('./OrganizationList.module.css')
+
+export class Navigation extends Component {
+  public render(): ReactElement {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default Navigation
