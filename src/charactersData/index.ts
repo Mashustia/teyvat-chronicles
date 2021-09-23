7b41f5def0b4c6dbb449d1a5a94a5d072682aebf
Mashustia <denisova.mariya.dm@gmail.com>
2021-09-24 00:42:38 +0300

    fix(charactersData): move file into src

---
+++ src/charactersData/index.ts
@@ -0,0 +1,32 @@
+const Amber = {
+  name: 'Amber',
+  vision: 'pyro'
+}
+
+const Barbara = {
+  name: 'Barbara',
+  vision: 'hydro'
+}
+
+const Beidou = {
+  name: 'Beidou',
+  vision: 'electro'
+}
+
+const Bennett = {
+  name: 'Bennett',
+  vision: 'pyro'
+}
+
+const Chongyun = {
+  name: 'Chongyun',
+  vision: 'cryo'
+}
+
+export default [
+  Amber,
+  Barbara,
+  Beidou,
+  Bennett,
+  Chongyun
+]
