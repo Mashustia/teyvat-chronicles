a787615ad6b9293172dde474575af3734a4e05b7
Mashustia <denisova.mariya.dm@gmail.com>
2021-09-25 16:14:09 +0300

    feat(character): add image and name

--- src/views/navigation/components/Character/index.tsx
+++ src/views/navigation/components/Character/index.tsx
@@ -1,10 +1,17 @@
 import { FC } from 'react'
 import {Col} from 'react-bootstrap';
 
+import {ICharacter} from '../../../../charactersData/types';
 
-const Character: FC = () => {
+const Character: FC<ICharacter> = ({ name }) => {
+  const imagePath = `src/images/${name}`
+
   return (
-    <Col xs={12} sm={6} md={2}>11111</Col>
+    <Col xs={12} sm={6} md={2}>
+      <img src={imagePath} alt={name} />
+      <h4>{name}</h4>
+
+    </Col>
   )
 }
 
