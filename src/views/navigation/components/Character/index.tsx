f77360c4e6251ab335b67eea230990ded161a74c
Mashustia <denisova.mariya.dm@gmail.com>
2021-09-25 17:19:38 +0300

    feat(character): add image style

--- src/views/navigation/components/Character/index.tsx
+++ src/views/navigation/components/Character/index.tsx
@@ -3,12 +3,14 @@ import {Col} from 'react-bootstrap';
 
 import {ICharacter} from '../../../../charactersData/types';
 
+import './character.css'
+
 const Character: FC<ICharacter> = ({ name }) => {
   const imagePath = `/images/characters/${name}.png`
 
   return (
     <Col xs={12} sm={6} md={2}>
-      <img src={imagePath} alt={name} />
+      <img src={imagePath} alt={name} className='character-img' />
       <h4>{name}</h4>
 
     </Col>
