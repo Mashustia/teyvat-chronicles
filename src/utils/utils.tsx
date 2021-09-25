4874b00ec8307b573e110abba034659f9dbeefb8
Mashustia <denisova.mariya.dm@gmail.com>
2021-09-25 18:31:33 +0300

    feat(utils): add createUrl function

---
+++ src/utils/utils.tsx
@@ -0,0 +1,28 @@
+import {match} from 'react-router-dom';
+
+import {IRouteParams} from '../types/commonTypes';
+
+export const getOrgDataFromMatch = (matchData: match<IRouteParams>): string => {
+  if (matchData?.params?.name) {
+    return `/${matchData.params.name}`
+  }
+
+  return ''
+}
+
+export const createUrl = (urlData: match<IRouteParams> | string, ...rest: Array<string | number>): string => {
+  const org = typeof urlData === 'string' ? `/${urlData}` : getOrgDataFromMatch(urlData)
+
+  return (
+    org +
+    rest.reduce((accumulator: string, currentValue: string | number) => {
+      if (!currentValue) return accumulator
+
+      const newCurrentValue =
+        typeof currentValue !== 'number' && !currentValue.includes('/') && accumulator.slice(-1) !== '/'
+          ? `/${currentValue}`
+          : currentValue
+      return `${accumulator}${newCurrentValue}`
+    }, '')
+  )
+}
