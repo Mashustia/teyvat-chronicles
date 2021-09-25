35c12336697406b8c232dc1023a3668ce69a0dc8
Mashustia <denisova.mariya.dm@gmail.com>
2021-09-26 00:05:06 +0300

    feat(tableItem): add component

---
+++ src/views/Character/components/TableItem/index.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react'
+import {Col, Row} from 'react-bootstrap';
+
+import {ITableItemProps as IProps} from './types';
+import {IMaterial} from '../../../../charactersData/types';
+
+const TableItem: FC<IProps> = ({data: [lvl, materials]}) => {
+  console.log(lvl, materials)
+
+  const rows = () => materials?.map(({material, count}: IMaterial) =>
+    <Col xs={3}><p>{material}</p><p>{count}</p></Col>)
+
+  return (
+    <Row>
+      <Col xs={2}>{lvl}</Col>
+      <Col xs={10}>
+        <Row>{rows}</Row>
+      </Col>
+    </Row>
+  )
+}
+
+export default TableItem
---
+++ src/views/Character/components/TableItem/types.ts
@@ -0,0 +1,5 @@
+import {IMaterial} from '../../../../charactersData/types';
+
+export type ITableItemProps = {
+  data: [string, IMaterial[]]
+}
