f159ca7801791d27cfa9b03906eb4f367a7bc291
Mashustia <denisova.mariya.dm@gmail.com>
2021-09-25 21:19:13 +0300

    feat(locales): add ascension_materials translation

--- src/views/Character/index.tsx
+++ src/views/Character/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import {withRouter} from 'react-router-dom';
 import {useTranslation} from 'react-i18next';
+import {Col, Row, Container} from 'react-bootstrap';
 
 import {ICharacterProps as IProps} from './types';
 
@@ -10,10 +11,21 @@ const Character: FC<IProps> = ({ match: {params}}) => {
   const imagePath = `/images/characters/${name}.png`
 
   return (
-    <div>
-      <img src={imagePath} alt={name} className='character-img' />
-      <h1>{t(`character:names.${name}`)}</h1>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col xs={12} md={2}>
+          <img src={imagePath} alt={name} className='character-img' />
+          <h1>{t(`character:names.${name}`)}</h1>
+        </Col>
+        <Col xs={12} md={10}>
+          <h4>{t('character:ascension_materials')}</h4>
+          <Row>
+            <Col xs={2}>1</Col>
+            <Col xs={10}>2</Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
