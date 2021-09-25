2154782dccbe42373819e190169d933002de6276
Mashustia <denisova.mariya.dm@gmail.com>
2021-09-25 19:32:37 +0300

    refactor(character): change withTranslation to useTranslation

--- src/views/Character/index.tsx
+++ src/views/Character/index.tsx
@@ -1,12 +1,18 @@
 import { FC } from 'react'
 import {withRouter} from 'react-router-dom';
+import {useTranslation} from 'react-i18next';
 
 import {ICharacterProps as IProps} from './types';
 
 const Character: FC<IProps> = ({ match: {params}}) => {
+  const { t } = useTranslation();
+  const { name } = params
+  const imagePath = `/images/characters/${name}.png`
+
   return (
     <div>
-      <h1>{params.name}</h1>
+      <img src={imagePath} alt={name} className='character-img' />
+      <h1>{t(`character:names.${name}`)}</h1>
     </div>
   )
 }
