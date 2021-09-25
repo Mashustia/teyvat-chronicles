ed28ef57568732a28eaeb3f952b0ec1d2d546a3c
Mashustia <denisova.mariya.dm@gmail.com>
2021-09-25 19:47:18 +0300

    style(header): add container

--- src/views/Header/Header.tsx
+++ src/views/Header/Header.tsx
@@ -1,19 +1,21 @@
 import {FC} from 'react'
-import {Col, Row} from 'react-bootstrap';
+import {Col, Container, Row} from 'react-bootstrap';
 import {useTranslation} from 'react-i18next';
 
 const Header: FC = (props) => {
   const { t } = useTranslation();
 
   return (
-    <header className='mb-2'>
-      <Row>
-        <Col>
-          <h1 className='h1'>
-            {t('header:welcome')}
-          </h1>
-        </Col>
-      </Row>
+    <header className='mb-3'>
+      <Container>
+        <Row>
+          <Col>
+            <h1 className='h1'>
+              {t('header:welcome')}
+            </h1>
+          </Col>
+        </Row>
+      </Container>
     </header>
   )
 }
