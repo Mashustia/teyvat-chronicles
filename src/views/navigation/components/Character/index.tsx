411243af552ced146b325fe8f0f79e9eb8920cb9
Mashustia <denisova.mariya.dm@gmail.com>
2021-09-25 17:44:14 +0300

    feat(app): add pointer class

--- src/views/navigation/components/Character/index.tsx
+++ src/views/navigation/components/Character/index.tsx
@@ -9,7 +9,7 @@ const Character: FC<ICharacter> = ({ name }) => {
   const imagePath = `/images/characters/${name}.png`
 
   return (
-    <Col xs={12} sm={6} md={2}>
+    <Col xs={12} sm={6} md={2} className='pointer'>
       <img src={imagePath} alt={name} className='character-img' />
       <h4>{name}</h4>
 
