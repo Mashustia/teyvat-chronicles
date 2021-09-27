a7aefbfa3b55c6e8119318452a34d57c3672ca02
Mashustia <denisova.mariya.dm@gmail.com>
2021-09-27 19:37:34 +0300

    feat(charactersData): add bennet

--- src/charactersData/index.ts
+++ src/charactersData/index.ts
@@ -1,11 +1,53 @@
 import {ICharacter} from './types';
 import {Vision} from '../const/consts';
-import {BossMaterials, Gem, Materials, MORA, Rocks} from './materialNames';
+import {BossMaterials, Flower, Gem, Materials, MORA, Rocks} from './materialNames';
 
 const Bennett = {
   name: 'Bennett',
   vision: Vision.PYRO,
-  ascension_materials: {},
+  ascension_materials: {
+    20: [
+      { material: Gem.PYRO.AGNIDUS_TOPAZ_SLIVER, count: 1 },
+      { material: Flower.WINDWHEEL_ASTER, count: 3 },
+      { material: Materials.TREASURE_HOARDER_INSIGNIA, count: 3 },
+      { material: MORA, count: 20000 },
+    ],
+    40: [
+      { material: Gem.PYRO.AGNIDUS_TOPAZ_FRAGMENT, count: 3 },
+      { material: BossMaterials.EVERFLAME_SEED, count: 2 },
+      { material: Flower.WINDWHEEL_ASTER, count: 10 },
+      { material: Materials.TREASURE_HOARDER_INSIGNIA, count: 15 },
+      { material: MORA, count: 40000 },
+    ],
+    50: [
+      { material: Gem.PYRO.AGNIDUS_TOPAZ_FRAGMENT, count: 6 },
+      { material: BossMaterials.EVERFLAME_SEED, count: 4 },
+      { material: Flower.WINDWHEEL_ASTER, count: 20 },
+      { material: Materials.SILVER_RAVEN_INSIGNIA, count: 12 },
+      { material: MORA, count: 20000 },
+    ],
+    60: [
+      { material: Gem.PYRO.AGNIDUS_TOPAZ_CHUNK, count: 3 },
+      { material: BossMaterials.EVERFLAME_SEED, count: 8 },
+      { material: Flower.WINDWHEEL_ASTER, count: 30 },
+      { material: Materials.SILVER_RAVEN_INSIGNIA, count: 18 },
+      { material: MORA, count: 20000 },
+    ],
+    70: [
+      { material: Gem.PYRO.AGNIDUS_TOPAZ_CHUNK, count: 6 },
+      { material: BossMaterials.EVERFLAME_SEED, count: 12 },
+      { material: Flower.WINDWHEEL_ASTER, count: 45 },
+      { material: Materials.GOLDEN_RAVEN_INSIGNIA, count: 12 },
+      { material: MORA, count: 20000 },
+    ],
+    80: [
+      { material: Gem.PYRO.AGNIDUS_TOPAZ_GEMSTONE, count: 6 },
+      { material: BossMaterials.EVERFLAME_SEED, count: 20 },
+      { material: Flower.WINDWHEEL_ASTER, count: 60 },
+      { material: Materials.GOLDEN_RAVEN_INSIGNIA, count: 24 },
+      { material: MORA, count: 120000 },
+    ],
+  },
   rarity: 4
 }
 
@@ -56,44 +98,44 @@ const Beidou = {
   vision: Vision.ELECTRO,
   ascension_materials: {
     20: [
-      { material: Gem.ELECTRO.vajrada_topaz_sliver, count: 1 },
-      { material: Rocks.noctilucous_jade, count: 3 },
-      { material: Materials.treasure_hoarder_insignia, count: 3 },
+      { material: Gem.ELECTRO.VAJRADA_TOPAZ_SLIVER, count: 1 },
+      { material: Rocks.NOCTILUCOUS_JADE, count: 3 },
+      { material: Materials.TREASURE_HOARDER_INSIGNIA, count: 3 },
       { material: MORA, count: 20000 },
     ],
     40: [
-      { material: Gem.ELECTRO.vajrada_topaz_fragment, count: 3 },
-      { material: BossMaterials.lightning_prism, count: 2 },
-      { material: Rocks.noctilucous_jade, count: 10 },
-      { material: Materials.treasure_hoarder_insignia, count: 15 },
+      { material: Gem.ELECTRO.VAJRADA_TOPAZ_FRAGMENT, count: 3 },
+      { material: BossMaterials.LIGHTNING_PRISM, count: 2 },
+      { material: Rocks.NOCTILUCOUS_JADE, count: 10 },
+      { material: Materials.TREASURE_HOARDER_INSIGNIA, count: 15 },
       { material: MORA, count: 40000 },
     ],
     50: [
-      { material: Gem.ELECTRO.vajrada_topaz_fragment, count: 6 },
-      { material: BossMaterials.lightning_prism, count: 4 },
-      { material: Rocks.noctilucous_jade, count: 20 },
-      { material: Materials.silver_raven_insignia, count: 12 },
+      { material: Gem.ELECTRO.VAJRADA_TOPAZ_FRAGMENT, count: 6 },
+      { material: BossMaterials.LIGHTNING_PRISM, count: 4 },
+      { material: Rocks.NOCTILUCOUS_JADE, count: 20 },
+      { material: Materials.SILVER_RAVEN_INSIGNIA, count: 12 },
       { material: MORA, count: 20000 },
     ],
     60: [
-      { material: Gem.ELECTRO.vajrada_topaz_chunk, count: 3 },
-      { material: BossMaterials.lightning_prism, count: 8 },
-      { material: Rocks.noctilucous_jade, count: 30 },
-      { material: Materials.silver_raven_insignia, count: 18 },
+      { material: Gem.ELECTRO.VAJRADA_TOPAZ_CHUNK, count: 3 },
+      { material: BossMaterials.LIGHTNING_PRISM, count: 8 },
+      { material: Rocks.NOCTILUCOUS_JADE, count: 30 },
+      { material: Materials.SILVER_RAVEN_INSIGNIA, count: 18 },
       { material: MORA, count: 20000 },
     ],
     70: [
-      { material: Gem.ELECTRO.vajrada_topaz_chunk, count: 6 },
-      { material: BossMaterials.lightning_prism, count: 12 },
-      { material: Rocks.noctilucous_jade, count: 45 },
-      { material: Materials.golden_raven_insignia, count: 12 },
+      { material: Gem.ELECTRO.VAJRADA_TOPAZ_CHUNK, count: 6 },
+      { material: BossMaterials.LIGHTNING_PRISM, count: 12 },
+      { material: Rocks.NOCTILUCOUS_JADE, count: 45 },
+      { material: Materials.GOLDEN_RAVEN_INSIGNIA, count: 12 },
       { material: MORA, count: 20000 },
     ],
     80: [
-      { material: Gem.ELECTRO.vajrada_topaz_gemstone, count: 6 },
-      { material: BossMaterials.lightning_prism, count: 20 },
-      { material: Rocks.noctilucous_jade, count: 60 },
-      { material: Materials.golden_raven_insignia, count: 24 },
+      { material: Gem.ELECTRO.VAJRADA_TOPAZ_GEMSTONE, count: 6 },
+      { material: BossMaterials.LIGHTNING_PRISM, count: 20 },
+      { material: Rocks.NOCTILUCOUS_JADE, count: 60 },
+      { material: Materials.GOLDEN_RAVEN_INSIGNIA, count: 24 },
       { material: MORA, count: 120000 },
     ],
   },
--- src/charactersData/materialNames.ts
+++ src/charactersData/materialNames.ts
@@ -1,55 +1,60 @@
 export const Gem = {
   GEO: {
-    prithiva_topaz_gemstone: "prithiva_topaz_gemstone",
-    prithiva_topaz_chunk: "prithiva_topaz_chunk",
-    prithiva_topaz_fragment: "prithiva_topaz_fragment",
-    prithiva_topaz_sliver: "prithiva_topaz_sliver"
+    PRITHIVA_TOPAZ_GEMSTONE: 'prithiva_topaz_gemstone',
+    PRITHIVA_TOPAZ_CHUNK: 'prithiva_topaz_chunk',
+    PRITHIVA_TOPAZ_FRAGMENT: 'prithiva_topaz_fragment',
+    PRITHIVA_TOPAZ_SLIVER: 'prithiva_topaz_sliver'
   },
   ANEMO: {
-    vayuda_topaz_gemstone:  "vayuda_topaz_gemstone",
-    vayuda_topaz_chunk:  "vayuda_topaz_chunk",
-    vayuda_topaz_fragment:  "vayuda_topaz_fragment",
-    vayuda_topaz_sliver:  "vayuda_topaz_sliver"
+    VAYUDA_TOPAZ_GEMSTONE:  'vayuda_topaz_gemstone',
+    VAYUDA_TOPAZ_CHUNK:  'vayuda_topaz_chunk',
+    VAYUDA_TOPAZ_FRAGMENT:  'vayuda_topaz_fragment',
+    VAYUDA_TOPAZ_SLIVER:  'vayuda_topaz_sliver'
   },
   CRYO: {
-    shivada_topaz_gemstone: "shivada_topaz_gemstone",
-    shivada_topaz_chunk: "shivada_topaz_chunk",
-    shivada_topaz_fragment: "shivada_topaz_fragment",
-    shivada_topaz_sliver: "shivada_topaz_sliver"
+    SHIVADA_TOPAZ_GEMSTONE: 'shivada_topaz_gemstone',
+    SHIVADA_TOPAZ_CHUNK: 'shivada_topaz_chunk',
+    SHIVADA_TOPAZ_FRAGMENT: 'shivada_topaz_fragment',
+    SHIVADA_TOPAZ_SLIVER: 'shivada_topaz_sliver'
   },
   ELECTRO: {
-    vajrada_topaz_gemstone: "vajrada_topaz_gemstone",
-    vajrada_topaz_chunk: "vajrada_topaz_chunk",
-    vajrada_topaz_fragment: "vajrada_topaz_fragment",
-    vajrada_topaz_sliver: "vajrada_topaz_sliver"
+    VAJRADA_TOPAZ_GEMSTONE: 'vajrada_topaz_gemstone',
+    VAJRADA_TOPAZ_CHUNK: 'vajrada_topaz_chunk',
+    VAJRADA_TOPAZ_FRAGMENT: 'vajrada_topaz_fragment',
+    VAJRADA_TOPAZ_SLIVER: 'vajrada_topaz_sliver'
 },
   HYDRO: {
-    varunada_topaz_gemstone: "varunada_topaz_gemstone",
-    varunada_topaz_chunk: "varunada_topaz_chunk",
-    varunada_topaz_fragment: "varunada_topaz_fragment",
-    varunada_topaz_sliver: "varunada_topaz_sliver"
+    VARUNADA_TOPAZ_GEMSTONE: 'varunada_topaz_gemstone',
+    VARUNADA_TOPAZ_CHUNK: 'varunada_topaz_chunk',
+    VARUNADA_TOPAZ_FRAGMENT: 'varunada_topaz_fragment',
+    VARUNADA_TOPAZ_SLIVER: 'varunada_topaz_sliver'
   },
   PYRO: {
-    agnidus_topaz_gemstone: "agnidus_topaz_gemstone",
-    agnidus_topaz_chunk: "agnidus_topaz_chunk",
-    agnidus_topaz_fragment: "agnidus_topaz_fragment",
-    agnidus_topaz_sliver: "agnidus_topaz_sliver"
+    AGNIDUS_TOPAZ_GEMSTONE: 'agnidus_topaz_gemstone',
+    AGNIDUS_TOPAZ_CHUNK: 'agnidus_topaz_chunk',
+    AGNIDUS_TOPAZ_FRAGMENT: 'agnidus_topaz_fragment',
+    AGNIDUS_TOPAZ_SLIVER: 'agnidus_topaz_sliver'
   }
 }
 
 export const MORA = 'mora'
 
-export const Rocks = {
-  noctilucous_jade: "noctilucous_jade"
+export enum Rocks {
+  NOCTILUCOUS_JADE = 'noctilucous_jade'
 }
 
-export const Materials = {
-  treasure_hoarder_insignia: "treasure_hoarder_insignia",
-  silver_raven_insignia: "silver_raven_insignia",
-  golden_raven_insignia: "golden_raven_insignia",
+export enum Materials {
+  TREASURE_HOARDER_INSIGNIA = 'treasure_hoarder_insignia',
+  SILVER_RAVEN_INSIGNIA = 'silver_raven_insignia',
+  GOLDEN_RAVEN_INSIGNIA = 'golden_raven_insignia',
 }
 
-export const BossMaterials = {
-  lightning_prism: "lightning_prism"
+export enum BossMaterials {
+  LIGHTNING_PRISM = 'lightning_prism',
+  EVERFLAME_SEED = 'everflame_seed'
+}
+
+export enum Flower {
+  WINDWHEEL_ASTER = 'windwheel_aster'
 }
 
