fec452fc7c7e7280e2e40fde19a43a5a28575763
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-01 23:51:46 +0300

    feat(amber): add amber talent materials

--- src/charactersData/characters/Amber.ts
+++ src/charactersData/characters/Amber.ts
@@ -1,5 +1,14 @@
 import {Vision} from '../../const/consts';
-import {BossMaterials, Flower, Gem, Materials, MORA} from '../materialNames';
+import {
+  Books,
+  BossMaterials,
+  BossSkillMaterial,
+  CROWN_OF_INSIGHT,
+  Flower,
+  Gem,
+  Materials,
+  MORA
+} from '../materialNames';
 
 const Amber = {
   name: 'Amber',
@@ -47,7 +56,59 @@ const Amber = {
       { material: MORA, count: 120000 },
     ]
   },
-  rarity: 4
+  rarity: 4,
+  talent_materials: {
+    2: [
+      {material: Books.TEACHINGS_OF_FREEDOM, count: 3},
+      {material: Materials.FIRM_ARROWHEAD, count: 6},
+      {material: MORA, count: 12500}
+    ],
+    3: [
+      {material: Books.GUIDE_TO_FREEDOM, count: 2},
+      {material: Materials.SHARP_ARROWHEAD, count: 3},
+      {material: MORA, count: 17500}
+    ],
+    4: [
+      {material: Books.GUIDE_TO_FREEDOM, count: 4},
+      {material: Materials.SHARP_ARROWHEAD, count: 4},
+      {material: MORA, count: 25000}
+    ],
+    5: [
+      {material: Books.GUIDE_TO_FREEDOM, count: 6},
+      {material: Materials.SHARP_ARROWHEAD, count: 6},
+      {material: MORA, count: 30000}
+    ],
+    6: [
+      {material: Books.GUIDE_TO_FREEDOM, count: 9},
+      {material: Materials.SHARP_ARROWHEAD, count: 9},
+      {material: MORA, count: 37500}
+    ],
+    7: [
+      {material: Books.PHILOSOPHIES_OF_FREEDOM, count: 4},
+      {material: Materials.WEATHERED_ARROWHEAD, count: 4},
+      {material: BossSkillMaterial.DVALINS_SIGH, count: 1},
+      {material: MORA, count: 120000}
+    ],
+    8: [
+      {material: Books.PHILOSOPHIES_OF_FREEDOM, count: 6},
+      {material: Materials.WEATHERED_ARROWHEAD, count: 6},
+      {material: BossSkillMaterial.DVALINS_SIGH, count: 1},
+      {material: MORA, count: 260000}
+    ],
+    9: [
+      {material: Books.PHILOSOPHIES_OF_FREEDOM, count: 12},
+      {material: Materials.WEATHERED_ARROWHEAD, count: 9},
+      {material: BossSkillMaterial.DVALINS_SIGH, count: 2},
+      {material: MORA, count: 450000}
+    ],
+    10: [
+      {material: Books.PHILOSOPHIES_OF_FREEDOM, count: 16},
+      {material: Materials.WEATHERED_ARROWHEAD, count: 12},
+      {material: BossSkillMaterial.DVALINS_SIGH, count: 2},
+      {material: CROWN_OF_INSIGHT, count: 1},
+      {material: MORA, count: 700000}
+    ]
+  }
 }
 
 export default Amber
