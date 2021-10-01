6879a3409701d8c65932c83c1d704722b849f39c
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-01 21:34:42 +0300

    refactor(Character): change old image with CharacterImage component

--- src/views/Characters/components/Character/index.tsx
+++ src/views/Characters/components/Character/index.tsx
@@ -2,30 +2,18 @@ import {FC} from 'react'
 import {Col} from 'react-bootstrap';
 import {withRouter} from 'react-router-dom';
 import {useTranslation} from 'react-i18next';
-import {Img} from 'react-image'
 
-import './Character.css'
 import {ICharacterProps as IProps} from './types';
 import {createUrl} from '../../../../utils/utils';
-import {ReactComponent as CharacterImageSkeleton} from '../CharacterImageSkeleton/CharacterImageSkeleton.svg'
+import CharacterImage from '../CharacterImage';
 
 const Character: FC<IProps> = ({name, history, match}) => {
   const {t} = useTranslation();
-  const imagePath = `/images/characters/${name}.png`
   const handleCharacterClick = () => history.push(createUrl(match, name))
-
-  const image = (
-    <Img
-      src={imagePath}
-      alt={name}
-      className='character-img img-border'
-      loader={<CharacterImageSkeleton/>}
-    />
-  )
 
   return (
     <Col xs={6} sm={3} md={3} lg={2} xl={2} xxl={1} className='pointer' onClick={handleCharacterClick}>
-      <div className='character-img-wrapper mx-auto'>{image}</div>
+      <CharacterImage name={name} withBorder={true}/>
       <h4>{t(`character:names.${name}`)}</h4>
     </Col>
   )
