b875e4c3d8fae11539bdd79696d2197214e7fc55
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-01 21:09:55 +0300

    feat(character): simplify component

--- src/views/Character/index.tsx
+++ src/views/Character/index.tsx
@@ -1,50 +1,20 @@
 import {FC, ReactElement, useEffect} from 'react'
-import {withRouter} from 'react-router-dom';
-import {useTranslation} from 'react-i18next';
-import {Col, Row, Container} from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 
-import {ICharacterProps as IProps} from './types';
-import {ICharacter} from '../../charactersData/types';
-import CHARACTERS from '../../charactersData';
-import AscensionMaterial from './components/AscensionMaterial';
 import Header from './components/Header';
+import AscensionMaterials from './components/AscensionMaterials';
 
-const Character: FC<IProps> = ({match: {params}, history}): ReactElement => {
+const Character: FC = (): ReactElement => {
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
-  const {t} = useTranslation();
-  const {name} = params
-  const imagePath = `/images/characters/${name}.png`
-  const activeCharacter = CHARACTERS.find((character: ICharacter) => character.name === params.name)
-
-  if (!activeCharacter) return <></>
-
-  const ascensionMaterials = Object.entries(activeCharacter.ascension_materials)
 
   return (
     <Container>
       <Header/>
-      <Row className='justify-content-center'>
-        <Col sm={12} md={12} lg={8} xl={7} xxl={6}>
-          <Row className='justify-content-center gx-3'>
-            <Col xs={12}>
-              <img src={imagePath} alt={name} className='character-img'/>
-              <h1 className='fs-3'>{t(`character:names.${name}`)}</h1>
-            </Col>
-            <Col xs={12}>
-              <h4 className='mb-3'>{t('character:ascension_materials')}</h4>
-              {ascensionMaterials.map((value, index) => {
-                if (!value[0]) return null
-
-                return <AscensionMaterial data={value} key={index}/>
-              })}
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+      <AscensionMaterials/>
     </Container>
   )
 }
 
-export default withRouter(Character)
+export default Character
