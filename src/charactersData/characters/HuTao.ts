3b8156c2f5f220a6b009f5fcaf95264e0917ded6
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-02 00:38:40 +0300

    feat(HuTao): add talent materials for HuTao

--- src/charactersData/characters/HuTao.ts
+++ src/charactersData/characters/HuTao.ts
@@ -1,5 +1,14 @@
 import {Vision} from '../../const/consts';
-import {BossMaterials, Flower, Gem, Materials, MORA} from '../materialNames';
+import {
+  Books,
+  BossMaterials,
+  BossSkillMaterial,
+  CROWN_OF_INSIGHT,
+  Flower,
+  Gem,
+  Materials,
+  MORA
+} from '../materialNames';
 
 const HuTao = {
   name: 'HuTao',
@@ -47,7 +56,59 @@ const HuTao = {
       { material: MORA, count: 120000 },
     ]
   },
-  rarity: 5
+  rarity: 5,
+  talent_materials: {
+    2: [
+      {material: Books.TEACHINGS_OF_DILIGENCE, count: 3},
+      {material: Materials.WHOPPERFLOWER_NECTAR, count: 6},
+      {material: MORA, count: 12500}
+    ],
+    3: [
+      {material: Books.GUIDE_TO_DILIGENCE, count: 2},
+      {material: Materials.SHIMMERING_NECTAR, count: 3},
+      {material: MORA, count: 17500}
+    ],
+    4: [
+      {material: Books.GUIDE_TO_DILIGENCE, count: 4},
+      {material: Materials.SHIMMERING_NECTAR, count: 4},
+      {material: MORA, count: 25000}
+    ],
+    5: [
+      {material: Books.GUIDE_TO_DILIGENCE, count: 6},
+      {material: Materials.SHIMMERING_NECTAR, count: 6},
+      {material: MORA, count: 30000}
+    ],
+    6: [
+      {material: Books.GUIDE_TO_DILIGENCE, count: 9},
+      {material: Materials.SHIMMERING_NECTAR, count: 9},
+      {material: MORA, count: 37500}
+    ],
+    7: [
+      {material: Books.PHILOSOPHIES_OF_DILIGENCE, count: 4},
+      {material: Materials.ENERGY_NECTAR, count: 4},
+      {material: BossSkillMaterial.SHARD_OF_A_FOUL_LEGACY, count: 1},
+      {material: MORA, count: 120000}
+    ],
+    8: [
+      {material: Books.PHILOSOPHIES_OF_DILIGENCE, count: 6},
+      {material: Materials.ENERGY_NECTAR, count: 6},
+      {material: BossSkillMaterial.SHARD_OF_A_FOUL_LEGACY, count: 1},
+      {material: MORA, count: 260000}
+    ],
+    9: [
+      {material: Books.PHILOSOPHIES_OF_DILIGENCE, count: 12},
+      {material: Materials.ENERGY_NECTAR, count: 9},
+      {material: BossSkillMaterial.SHARD_OF_A_FOUL_LEGACY, count: 2},
+      {material: MORA, count: 450000}
+    ],
+    10: [
+      {material: Books.PHILOSOPHIES_OF_DILIGENCE, count: 16},
+      {material: Materials.ENERGY_NECTAR, count: 12},
+      {material: BossSkillMaterial.SHARD_OF_A_FOUL_LEGACY, count: 2},
+      {material: CROWN_OF_INSIGHT, count: 1},
+      {material: MORA, count: 700000}
+    ]
+  }
 }
 
 export default HuTao
