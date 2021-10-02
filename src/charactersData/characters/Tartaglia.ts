d6b764a825bac1c0fa8830ad5b56084f79b96582
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-02 10:55:58 +0300

    feat(Tartaglia): add talent materials for Tartaglia

--- src/charactersData/characters/Tartaglia.ts
+++ src/charactersData/characters/Tartaglia.ts
@@ -1,5 +1,5 @@
 import {Vision} from '../../const/consts';
-import {BossMaterials, Rocks, Gem, Materials, MORA} from '../materialNames';
+import {BossMaterials, Rocks, Gem, Materials, MORA, Books, BossSkillMaterial, CROWN_OF_INSIGHT} from '../materialNames';
 
 const Tartaglia = {
   name: 'Tartaglia',
@@ -47,7 +47,59 @@ const Tartaglia = {
       { material: MORA, count: 120000 },
     ]
   },
-  rarity: 5
+  rarity: 5,
+  talent_materials: {
+    2: [
+      {material: Books.TEACHINGS_OF_FREEDOM, count: 3},
+      {material: Materials.RECRUITS_INSIGNIA, count: 6},
+      {material: MORA, count: 12500}
+    ],
+    3: [
+      {material: Books.GUIDE_TO_FREEDOM, count: 2},
+      {material: Materials.SERGEANTS_INSIGNIA, count: 3},
+      {material: MORA, count: 17500}
+    ],
+    4: [
+      {material: Books.GUIDE_TO_FREEDOM, count: 4},
+      {material: Materials.SERGEANTS_INSIGNIA, count: 4},
+      {material: MORA, count: 25000}
+    ],
+    5: [
+      {material: Books.GUIDE_TO_FREEDOM, count: 6},
+      {material: Materials.SERGEANTS_INSIGNIA, count: 6},
+      {material: MORA, count: 30000}
+    ],
+    6: [
+      {material: Books.GUIDE_TO_FREEDOM, count: 9},
+      {material: Materials.SERGEANTS_INSIGNIA, count: 9},
+      {material: MORA, count: 37500}
+    ],
+    7: [
+      {material: Books.PHILOSOPHIES_OF_FREEDOM, count: 4},
+      {material: Materials.LIEUTENANTS_INSIGNIA, count: 4},
+      {material: BossSkillMaterial.SHARD_OF_A_FOUL_LEGACY, count: 1},
+      {material: MORA, count: 120000}
+    ],
+    8: [
+      {material: Books.PHILOSOPHIES_OF_FREEDOM, count: 6},
+      {material: Materials.LIEUTENANTS_INSIGNIA, count: 6},
+      {material: BossSkillMaterial.SHARD_OF_A_FOUL_LEGACY, count: 1},
+      {material: MORA, count: 260000}
+    ],
+    9: [
+      {material: Books.PHILOSOPHIES_OF_FREEDOM, count: 12},
+      {material: Materials.LIEUTENANTS_INSIGNIA, count: 9},
+      {material: BossSkillMaterial.SHARD_OF_A_FOUL_LEGACY, count: 2},
+      {material: MORA, count: 450000}
+    ],
+    10: [
+      {material: Books.PHILOSOPHIES_OF_FREEDOM, count: 16},
+      {material: Materials.LIEUTENANTS_INSIGNIA, count: 12},
+      {material: BossSkillMaterial.SHARD_OF_A_FOUL_LEGACY, count: 2},
+      {material: CROWN_OF_INSIGHT, count: 1},
+      {material: MORA, count: 700000}
+    ]
+  }
 }
 
 export default Tartaglia
