ce0d3eefd57a77a68ae81fb2a372501ae6975fe8
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-02 11:01:15 +0300

    feat(Xiao): add talent materials for Xiao

--- src/charactersData/characters/Xiao.ts
+++ src/charactersData/characters/Xiao.ts
@@ -1,5 +1,14 @@
 import {Vision} from '../../const/consts';
-import {BossMaterials, Flower, Gem, Materials, MORA} from '../materialNames';
+import {
+  Books,
+  BossMaterials,
+  BossSkillMaterial,
+  CROWN_OF_INSIGHT,
+  Flower,
+  Gem,
+  Materials,
+  MORA
+} from '../materialNames';
 
 const Xiao = {
   name: 'Xiao',
@@ -47,7 +56,59 @@ const Xiao = {
       { material: MORA, count: 120000 },
     ]
   },
-  rarity: 5
+  rarity: 5,
+  talent_materials: {
+    2: [
+      {material: Books.TEACHINGS_OF_PROSPERITY, count: 3},
+      {material: Materials.SLIME_CONDENSATE, count: 6},
+      {material: MORA, count: 12500}
+    ],
+    3: [
+      {material: Books.GUIDE_TO_PROSPERITY, count: 2},
+      {material: Materials.SLIME_SECRETIONS, count: 3},
+      {material: MORA, count: 17500}
+    ],
+    4: [
+      {material: Books.GUIDE_TO_PROSPERITY, count: 4},
+      {material: Materials.SLIME_SECRETIONS, count: 4},
+      {material: MORA, count: 25000}
+    ],
+    5: [
+      {material: Books.GUIDE_TO_PROSPERITY, count: 6},
+      {material: Materials.SLIME_SECRETIONS, count: 6},
+      {material: MORA, count: 30000}
+    ],
+    6: [
+      {material: Books.GUIDE_TO_PROSPERITY, count: 9},
+      {material: Materials.SLIME_SECRETIONS, count: 9},
+      {material: MORA, count: 37500}
+    ],
+    7: [
+      {material: Books.PHILOSOPHIES_OF_PROSPERITY, count: 4},
+      {material: Materials.SLIME_CONCENTRATE, count: 4},
+      {material: BossSkillMaterial.SHADOW_OF_THE_WARRIOR, count: 1},
+      {material: MORA, count: 120000}
+    ],
+    8: [
+      {material: Books.PHILOSOPHIES_OF_PROSPERITY, count: 6},
+      {material: Materials.SLIME_CONCENTRATE, count: 6},
+      {material: BossSkillMaterial.SHADOW_OF_THE_WARRIOR, count: 1},
+      {material: MORA, count: 260000}
+    ],
+    9: [
+      {material: Books.PHILOSOPHIES_OF_PROSPERITY, count: 12},
+      {material: Materials.SLIME_CONCENTRATE, count: 9},
+      {material: BossSkillMaterial.SHADOW_OF_THE_WARRIOR, count: 2},
+      {material: MORA, count: 450000}
+    ],
+    10: [
+      {material: Books.PHILOSOPHIES_OF_PROSPERITY, count: 16},
+      {material: Materials.SLIME_CONCENTRATE, count: 12},
+      {material: BossSkillMaterial.SHADOW_OF_THE_WARRIOR, count: 2},
+      {material: CROWN_OF_INSIGHT, count: 1},
+      {material: MORA, count: 700000}
+    ]
+  }
 }
 
 export default Xiao
