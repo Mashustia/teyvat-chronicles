de40a09c614186b824df513f9a31ee85651a1934
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-03 09:30:34 +0300

    feat(Yoimiya): add talent materials for Yoimiya

--- src/charactersData/characters/Yoimiya.ts
+++ src/charactersData/characters/Yoimiya.ts
@@ -1,5 +1,14 @@
 import {Vision} from '../../const/consts';
-import {BossMaterials, Flower, Gem, Materials, MORA} from '../materialNames';
+import {
+  Books,
+  BossMaterials,
+  BossSkillMaterial,
+  CROWN_OF_INSIGHT,
+  Flower,
+  Gem,
+  Materials,
+  MORA
+} from '../materialNames';
 
 const Yoimiya = {
   name: 'Yoimiya',
@@ -47,7 +56,59 @@ const Yoimiya = {
       { material: MORA, count: 120000 },
     ]
   },
-  rarity: 5
+  rarity: 5,
+  talent_materials: {
+    2: [
+      {material: Books.TEACHINGS_OF_TRANSIENCE, count: 3},
+      {material: Materials.DIVINING_SCROLL, count: 6},
+      {material: MORA, count: 12500}
+    ],
+    3: [
+      {material: Books.GUIDE_TO_TRANSIENCE, count: 2},
+      {material: Materials.SEALED_SCROLL, count: 3},
+      {material: MORA, count: 17500}
+    ],
+    4: [
+      {material: Books.GUIDE_TO_TRANSIENCE, count: 4},
+      {material: Materials.SEALED_SCROLL, count: 4},
+      {material: MORA, count: 25000}
+    ],
+    5: [
+      {material: Books.GUIDE_TO_TRANSIENCE, count: 6},
+      {material: Materials.SEALED_SCROLL, count: 6},
+      {material: MORA, count: 30000}
+    ],
+    6: [
+      {material: Books.GUIDE_TO_TRANSIENCE, count: 9},
+      {material: Materials.SEALED_SCROLL, count: 9},
+      {material: MORA, count: 37500}
+    ],
+    7: [
+      {material: Books.PHILOSOPHIES_OF_TRANSIENCE, count: 4},
+      {material: Materials.FORBIDDEN_CURSE_SCROLL, count: 4},
+      {material: BossSkillMaterial.DRAGON_LORDS_CROWN, count: 1},
+      {material: MORA, count: 120000}
+    ],
+    8: [
+      {material: Books.PHILOSOPHIES_OF_TRANSIENCE, count: 6},
+      {material: Materials.FORBIDDEN_CURSE_SCROLL, count: 6},
+      {material: BossSkillMaterial.DRAGON_LORDS_CROWN, count: 1},
+      {material: MORA, count: 260000}
+    ],
+    9: [
+      {material: Books.PHILOSOPHIES_OF_TRANSIENCE, count: 12},
+      {material: Materials.FORBIDDEN_CURSE_SCROLL, count: 9},
+      {material: BossSkillMaterial.DRAGON_LORDS_CROWN, count: 2},
+      {material: MORA, count: 450000}
+    ],
+    10: [
+      {material: Books.PHILOSOPHIES_OF_TRANSIENCE, count: 16},
+      {material: Materials.FORBIDDEN_CURSE_SCROLL, count: 12},
+      {material: BossSkillMaterial.DRAGON_LORDS_CROWN, count: 2},
+      {material: CROWN_OF_INSIGHT, count: 1},
+      {material: MORA, count: 700000}
+    ]
+  }
 }
 
 export default Yoimiya
