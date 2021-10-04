eb324d3bea72ad5731dd2ef69e60f1ec6c86daa9
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-05 00:35:56 +0300

    feat(Footer): add rights field

--- src/common/Footer/index.tsx
+++ src/common/Footer/index.tsx
@@ -6,11 +6,12 @@ import {useTranslation} from 'react-i18next';
 const Footer: FC = () => {
   const {t} = useTranslation();
   return (
-    <footer className='footer py-2'>
+    <footer className='footer p-2'>
       <div className='d-flex flex-nowrap justify-content-center'>
         <p className='d-block mb-0 me-1'>{t('footer:contacts')} </p>
         <a href='mailto: marshmallowgoon21@gmail.com' className='footer-link'>marshmallowgoon21@gmail.com</a>
       </div>
+      <p className='d-flex flex-nowrap justify-content-center mb-0 footer-rights'>{t('footer:rights')}</p>
     </footer>
   )
 }
