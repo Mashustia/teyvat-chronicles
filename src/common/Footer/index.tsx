f2f5c76a781842e83ef735521a973c118887efd3
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-05 00:30:07 +0300

    feat(Footer): add contacts field

--- src/common/Footer/index.tsx
+++ src/common/Footer/index.tsx
@@ -1,11 +1,16 @@
 import {FC} from 'react'
 
 import './Footer.css'
+import {useTranslation} from 'react-i18next';
 
 const Footer: FC = () => {
+  const {t} = useTranslation();
   return (
     <footer className='footer py-2'>
-      <a href='mailto: marshmallowgoon21@gmail.com' className='footer-link'>marshmallowgoon21@gmail.com</a>
+      <div className='d-flex flex-nowrap justify-content-center'>
+        <p className='d-block mb-0 me-1'>{t('footer:contacts')} </p>
+        <a href='mailto: marshmallowgoon21@gmail.com' className='footer-link'>marshmallowgoon21@gmail.com</a>
+      </div>
     </footer>
   )
 }
--- src/i18n.ts
+++ src/i18n.ts
@@ -5,11 +5,13 @@ import { initReactI18next } from 'react-i18next'
 import {Languages, NS} from './const/consts';
 import HEADER from './locales/ru/header.json';
 import CHARACTER from './locales/ru/character.json';
+import FOOTER from './locales/ru/footer.json';
 
 const Translations = {
   ru: {
     header: HEADER,
-    character: CHARACTER
+    character: CHARACTER,
+    footer: FOOTER
   }
 };
 
