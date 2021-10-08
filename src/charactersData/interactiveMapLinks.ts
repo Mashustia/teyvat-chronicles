be793e71c980a96f22daaf5fc87d9928b7c3dc4a
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-09 00:57:04 +0300

    feat(interactiveMaoLinks): add links for flowers

---
+++ src/charactersData/interactiveMapLinks.ts
@@ -0,0 +1,20 @@
+export const materialLink = {
+  windwheel_aster: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=43&center=-86.05,-488.44&zoom=-0.50',
+  philanemo_mushroom: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=39&center=196.25,31.00&zoom=1.00',
+  qingxin: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=32&center=1968.00,-1292.00&zoom=-1.00',
+  valberry: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=29&center=-449.86,524.14&zoom=0.50',
+  small_lamp_grass: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=33&center=48.00,-336.00&zoom=-1.00',
+  glaze_lily: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=38&center=2458.12,-1305.86&zoom=-0.50',
+  calla_lily: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=31&center=116.18,-311.66&zoom=-0.50',
+  silk_flower: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=36&center=2040.93,-1434.55&zoom=-0.50',
+  jueyun_chili: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=30&center=1244.72,-1865.88&zoom=-0.50',
+  dandelion_seed: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=37&center=114.77,112.60&zoom=-0.50',
+  amakumo_fruit: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=228&center=6832.41,3221.06&zoom=-0.50',
+  wolfhook: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=41&center=334.50,-373.00&zoom=-0.50',
+  cecilia: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=35&center=-262.83,652.83&zoom=-0.50',
+  violetgrass: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=34&center=1509.18,-976.34&zoom=-0.50',
+  sea_ganoderma: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=185&center=5622.00,1888.00&zoom=-1.00',
+  sakura_bloom: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=199&center=4980.02,3566.82&zoom=0.50',
+  naku_weed: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=197&center=6819.20,3300.94&zoom=0.50',
+  dendrobium: 'https://webstatic-sea.mihoyo.com/app/ys-map-sea/?lang=en-us#/map/2?shown_types=198&center=5811.50,1922.00&zoom=0.00'
+}
