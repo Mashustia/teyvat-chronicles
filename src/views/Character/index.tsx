119c8534f5adc33db7de6003ea6ed13d363679b3
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-09 15:12:23 +0300

    fix(character): delete useEffect activation for scroll

--- src/views/Character/index.tsx
+++ src/views/Character/index.tsx
@@ -1,18 +1,12 @@
-import {FC, ReactElement, useEffect} from 'react'
+import {FC, ReactElement} from 'react'
 import {Container} from 'react-bootstrap';
 
 import Materials from './components/Materials';
 
-const Character: FC = (): ReactElement => {
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
-
-  return (
-    <Container>
-      <Materials/>
-    </Container>
-  )
-}
+const Character: FC = (): ReactElement => (
+  <Container>
+    <Materials/>
+  </Container>
+)
 
 export default Character
