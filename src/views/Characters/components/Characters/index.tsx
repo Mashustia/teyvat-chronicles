42803d4aa7463d8382bbf6dda0db00416d9227a5
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-09 17:56:31 +0300

    feat(characters): translate name and vision

--- src/views/Characters/components/Characters/index.tsx
+++ src/views/Characters/components/Characters/index.tsx
@@ -5,15 +5,28 @@ import {groupBy, sortBy} from 'lodash'
 import Character from '../Character';
 import CHARACTERS from '../../../../charactersData';
 import {ICharacter} from '../../../../charactersData/types';
+import {useTranslation} from 'react-i18next';
 
 enum groupField {
   VISION = 'vision',
-  NAME = 'name'
+  NAME = 'name',
+  TRANSLATED_NAME = 'translated_name'
 }
 
-const sortedCharacters = sortBy(CHARACTERS, [groupField.VISION, groupField.NAME])
-
 const Characters: FC = () => {
+  const {t} = useTranslation()
+
+  const characterWithTranslatedNames = CHARACTERS.map((character: ICharacter) => {
+    const {name, vision} = character
+    return ({
+      ...character,
+      translated_name: t(`character:names.${name}`),
+      translated_vision: t(`character:vision.${vision}`)
+    })
+  })
+
+  const sortedCharacters = sortBy(characterWithTranslatedNames, [groupField.VISION, groupField.TRANSLATED_NAME])
+
   const charactersByVision = groupBy(sortedCharacters, groupField.VISION)
 
   const groupedCharacterKeys = Object.keys(charactersByVision)
