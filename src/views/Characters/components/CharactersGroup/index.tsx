d70b9437d72c87a016a4fd00e1cf94fdb0d54462
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-09 20:24:42 +0300

    feat(charactersGroup): add not found message

--- src/views/Characters/components/CharactersGroup/index.tsx
+++ src/views/Characters/components/CharactersGroup/index.tsx
@@ -5,6 +5,7 @@ import {groupBy, sortBy} from 'lodash'
 import Character from '../Character';
 import {ICharactersGroup as IProps} from './types';
 import {ICharacterProps as ICharacter} from '../Character/types';
+import {useTranslation} from 'react-i18next';
 
 export enum groupField {
   VISION = 'vision',
@@ -13,6 +14,14 @@ export enum groupField {
 }
 
 const CharactersGroup: FC<IProps> = ({characters}) => {
+  const {t} = useTranslation('character')
+
+  if (characters.length === 0) {
+    return (
+      <p className='fs-5'>{t('character:character_not_found')}</p>
+    )
+  }
+
   const sortedCharacters = sortBy(characters, [groupField.VISION, groupField.TRANSLATED_NAME])
   const charactersByVision = groupBy(sortedCharacters, groupField.VISION)
   const groupedCharacterKeys = Object.keys(charactersByVision)
