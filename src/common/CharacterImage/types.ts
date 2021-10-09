2e6a65960afc43469b77cbb30ab16239c6ae16c0
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-09 15:08:24 +0300

    feat(character): add link wrapper

--- src/common/CharacterImage/types.ts
+++ src/common/CharacterImage/types.ts
@@ -1,4 +1,9 @@
-export interface ICharacterImageProps {
+import {RouteComponentProps} from 'react-router-dom';
+
+import {IRouteParams} from '../../types/commonTypes';
+
+export interface ICharacterImageProps extends RouteComponentProps<IRouteParams> {
   name: string
   withBorder?: boolean
+  withLink?: boolean
 }
