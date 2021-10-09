7fb90cba2b627d96a5c99965cc896c5ec0113314
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-09 19:54:49 +0300

    refactor(layout): refactor Header import

--- src/views/Layout/index.tsx
+++ src/views/Layout/index.tsx
@@ -1,7 +1,7 @@
 import {FC} from 'react'
 import {Container} from 'react-bootstrap';
 
-import Header from '../Header/Header';
+import Header from '../Header';
 import Characters from '../Characters/components/Characters';
 
 const Layout: FC = () => {
