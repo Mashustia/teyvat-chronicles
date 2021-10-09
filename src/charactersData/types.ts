7e3ff53afe039aa6f626c24471ef0fe50e2a0da3
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-09 17:53:01 +0300

    feat(types): add translated_name and translated_vision

--- src/charactersData/types.ts
+++ src/charactersData/types.ts
@@ -6,6 +6,8 @@ export interface ICharacter {
   ascension_materials: IAscensionMaterials
   rarity: number
   talent_materials?: IAscensionMaterials
+  translated_name?: string
+  translated_vision?: string
 }
 
 export interface IAscensionMaterials {
