28f9700a238fa35eaed66fdda96f56362f0b8149
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-09 20:12:12 +0300

    style(character): delete xxl rules for column

--- src/views/Characters/components/Character/index.tsx
+++ src/views/Characters/components/Character/index.tsx
@@ -6,7 +6,7 @@ import Stars from '../../../../common/Stars';
 import {ICharacterProps as IProps} from './types';
 
 const Character: FC<IProps> = ({name, rarity, translated_name}) => (
-  <Col xs={6} sm={3} md={3} lg={2} xl={2} xxl={1}>
+  <Col xs={6} sm={3} md={3} lg={2} xl={2}>
     <CharacterImage name={name} withBorder={true} withLink={true}/>
     <h4 className='d-flex justify-content-center mb-0'>{translated_name}</h4>
     <div className='mb-3'>{<Stars count={rarity}/>}</div>
