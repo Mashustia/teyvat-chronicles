090b269e84987c5c2097645fe70ff39097a46694
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-09 17:55:45 +0300

    fix(character): add translated_name props

--- src/views/Characters/components/Character/index.tsx
+++ src/views/Characters/components/Character/index.tsx
@@ -1,21 +1,16 @@
 import {FC} from 'react'
 import {Col} from 'react-bootstrap';
-import {useTranslation} from 'react-i18next';
 
 import CharacterImage from '../../../../common/CharacterImage';
 import {ICharacter as IProps} from '../../../../charactersData/types';
 import Stars from '../../../../common/Stars';
 
-const Character: FC<IProps> = ({name, rarity}) => {
-  const {t} = useTranslation();
-
-  return (
-    <Col xs={6} sm={3} md={3} lg={2} xl={2} xxl={1}>
-      <CharacterImage name={name} withBorder={true} withLink={true}/>
-      <h4 className='d-flex justify-content-center mb-0'>{t(`character:names.${name}`)}</h4>
-      <div className='mb-3'>{<Stars count={rarity}/>}</div>
-    </Col>
-  )
-}
+const Character: FC<IProps> = ({name, rarity, translated_name}) => (
+  <Col xs={6} sm={3} md={3} lg={2} xl={2} xxl={1}>
+    <CharacterImage name={name} withBorder={true} withLink={true}/>
+    <h4 className='d-flex justify-content-center mb-0'>{translated_name}</h4>
+    <div className='mb-3'>{<Stars count={rarity}/>}</div>
+  </Col>
+)
 
 export default Character
