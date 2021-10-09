069852aa4dca4dae0a4a4ad4cd9fbca864a73231
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-09 19:55:18 +0300

    style(main): add margin bottom

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,14 +1,14 @@
 import {FC} from 'react'
 import {Route, Switch} from 'react-router-dom';
 
-import './Main.css'
 import {RouteName} from '../../const/consts';
 import Layout from '../Layout';
 import Character from '../Character';
+import './Main.css'
 
 const Main: FC = (children) => {
   return (
-    <main role='main'>
+    <main role='main' className='main'>
       <Switch>
         <Route exact path={RouteName.DEFAULT} component={Layout} />
         <Route exact path={RouteName.CHARACTER} component={Character} />
