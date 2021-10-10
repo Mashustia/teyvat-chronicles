10a9e5a23c79b6704d3086e221491aaa822aab17
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-11 01:38:56 +0300

    style(character): add name container auto margin

--- src/views/Characters/components/Character/index.tsx
+++ src/views/Characters/components/Character/index.tsx
@@ -1,16 +1,15 @@
 import {FC} from 'react'
-import {Col} from 'react-bootstrap';
 
 import CharacterImage from '../../../../common/CharacterImage';
 import Stars from '../../../../common/Stars';
 import {ICharacterProps as IProps} from './types';
 
 const Character: FC<IProps> = ({name, rarity, translated_name}) => (
-  <Col xs={6} sm={3} md={3} lg={2} xl={2}>
+  <div className='d-flex flex-column character'>
     <CharacterImage name={name} withBorder={true} withLink={true}/>
-    <h4 className='d-flex justify-content-center mb-0'>{translated_name}</h4>
-    <div className='mb-3'>{<Stars count={rarity}/>}</div>
-  </Col>
+    <h4 className='d-flex justify-content-center my-auto'>{translated_name}</h4>
+    <div>{<Stars count={rarity}/>}</div>
+  </div>
 )
 
 export default Character
