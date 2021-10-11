e3cb40705f161a5e1cafabde78e2bb914bd51d02
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-11 12:07:44 +0300

    style(Footer): delete nowrap

--- src/common/Footer/index.tsx
+++ src/common/Footer/index.tsx
@@ -7,7 +7,7 @@ const Footer: FC = () => {
   const {t} = useTranslation();
   return (
     <footer className='footer p-2'>
-      <div className='d-flex flex-nowrap justify-content-center'>
+      <div className='d-flex justify-content-center'>
         <p className='d-block mb-0 me-1'>{t('footer:contacts')} </p>
         <a href='mailto: marshmallowgoon21@gmail.com' className='footer-link'>marshmallowgoon21@gmail.com</a>
       </div>
