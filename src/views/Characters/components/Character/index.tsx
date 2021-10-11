ac1c0b50f1891e227d36e2feb959a21349dab0b1
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-11 12:01:35 +0300

    style(character): add style import

--- src/views/Characters/components/Character/index.tsx
+++ src/views/Characters/components/Character/index.tsx
@@ -3,9 +3,10 @@ import {FC} from 'react'
 import CharacterImage from '../../../../common/CharacterImage';
 import Stars from '../../../../common/Stars';
 import {ICharacterProps as IProps} from './types';
+import './Character.css'
 
 const Character: FC<IProps> = ({name, rarity, translated_name}) => (
-  <div className='d-flex flex-column character'>
+  <div className='d-flex flex-column character mx-3'>
     <CharacterImage name={name} withBorder={true} withLink={true}/>
     <h4 className='d-flex justify-content-center my-auto'>{translated_name}</h4>
     <div>{<Stars count={rarity}/>}</div>
