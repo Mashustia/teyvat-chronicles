f3eee920e512d400219540d17f9f811d5b070942
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-19 22:20:20 +0300

    fix(footer): add trademark info in footer

--- src/common/Footer/index.tsx
+++ src/common/Footer/index.tsx
@@ -3,12 +3,15 @@ import {FC} from 'react'
 import {useTranslation} from 'react-i18next';
 
 const Footer: FC = () => {
-  const {t} = useTranslation();
+  const {t} = useTranslation('footer');
   return (
     <footer className='p-2'>
       <div className='d-flex justify-content-center'>
-        <p className='d-block mb-0 me-1'>{t('footer:contacts')} </p>
+        <p className='d-block mb-0 me-1 mb-2'>{t('footer:contacts')}</p>
         <a href='mailto: marshmallowgoon21@gmail.com' className='link'>marshmallowgoon21@gmail.com</a>
+      </div>
+      <div className='d-flex justify-content-center'>
+        <p className='d-block mb-0'>{t('footer:trademark')}</p>
       </div>
     </footer>
   )
