119d8557fc3f41dd3b4792557f3ca8d653b2b47b
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-19 23:39:05 +0300

    feat(locales): add common translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -10,6 +10,7 @@ import FOOTER_RU from './locales/ru/footer.json';
 import LANGUAGE_RU from './locales/ru/language.json';
 import MATERIALS_RU from './locales/ru/materials.json';
 import MATERIAL_RU from './locales/ru/material.json';
+import COMMON_RU from './locales/ru/common.json';
 
 import HEADER_EN from './locales/en/header.json';
 import CHARACTER_EN from './locales/en/character.json';
@@ -17,6 +18,7 @@ import FOOTER_EN from './locales/en/footer.json';
 import LANGUAGE_EN from './locales/en/language.json';
 import MATERIALS_EN from './locales/en/materials.json';
 import MATERIAL_EN from './locales/en/material.json';
+import COMMON_EN from './locales/en/common.json';
 
 const Translations = {
   ru: {
@@ -25,7 +27,8 @@ const Translations = {
     footer: FOOTER_RU,
     language: LANGUAGE_RU,
     materials: MATERIALS_RU,
-    material: MATERIAL_RU
+    material: MATERIAL_RU,
+    common: COMMON_RU
   },
   en: {
     header: HEADER_EN,
@@ -33,7 +36,8 @@ const Translations = {
     footer: FOOTER_EN,
     language: LANGUAGE_EN,
     materials: MATERIALS_EN,
-    material: MATERIAL_EN
+    material: MATERIAL_EN,
+    common: COMMON_EN
   }
 };
 
