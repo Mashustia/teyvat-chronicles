aae2588a1c1f9e46b6c3f8f3e09c56d59b71343d
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-21 22:10:31 +0300

    feat(material): add Freedom books

--- src/charactersData/dungeons.ts
+++ src/charactersData/dungeons.ts
@@ -8,8 +8,22 @@ export enum Region {
 const ForsakenRift = {
   name: 'forsaken_rift',
   region: Region.MONDSTADT,
-  is_open_days: [WeekDays.WEDNESDAY, WeekDays.SATURDAY, WeekDays.SUNDAY],
   original_resin: 20
+}
+
+const Ballad = {
+  ...ForsakenRift,
+  is_open_days: [WeekDays.WEDNESDAY, WeekDays.SATURDAY, WeekDays.SUNDAY]
+}
+
+const Resistance = {
+  ...ForsakenRift,
+  is_open_days: [WeekDays.TUESDAY, WeekDays.FRIDAY, WeekDays.SUNDAY]
+}
+
+const Freedom = {
+  ...ForsakenRift,
+  is_open_days: [WeekDays.MONDAY, WeekDays.THURSDAY, WeekDays.SUNDAY]
 }
 
 export const Dungeon: IDungeon = {
@@ -18,7 +32,13 @@ export const Dungeon: IDungeon = {
     is_open_days: [WeekDays.ALL_DAYS],
     original_resin: 20
   },
-  teachings_of_ballad: { ...ForsakenRift },
-  guide_to_ballad: { ...ForsakenRift },
-  philosophies_of_ballad: { ...ForsakenRift }
+  teachings_of_ballad: {...Ballad},
+  guide_to_ballad: {...Ballad},
+  philosophies_of_ballad: {...Ballad},
+  teachings_of_resistance: {...Resistance},
+  guide_to_resistance: {...Resistance},
+  philosophies_of_resistance: {...Resistance},
+  teachings_of_freedom: {...Freedom},
+  guide_to_freedom: {...Freedom},
+  philosophies_of_freedom: {...Freedom}
 }
