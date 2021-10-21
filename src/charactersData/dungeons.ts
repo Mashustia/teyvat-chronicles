5915067332fd96b1ccbbeb5367f228b0a29a7783
Mashustia <denisova.mariya.dm@gmail.com>
2021-10-21 22:26:42 +0300

    feat(material): add Taishan Mansion dungeon

--- src/charactersData/dungeons.ts
+++ src/charactersData/dungeons.ts
@@ -2,7 +2,8 @@ import {IDungeon} from '../views/Character/components/Material/types';
 import {WeekDays} from '../const/consts';
 
 export enum Region {
-  MONDSTADT = 'monstadt'
+  MONDSTADT = 'monstadt',
+  LIYUE = 'liyue'
 }
 
 const ForsakenRift = {
@@ -26,6 +27,27 @@ const Freedom = {
   is_open_days: [WeekDays.MONDAY, WeekDays.THURSDAY, WeekDays.SUNDAY]
 }
 
+const TaishanMansion = {
+  name: 'taishan_mansion',
+  region: Region.LIYUE,
+  original_resin: 20
+}
+
+const Prosperity = {
+  ...TaishanMansion,
+  is_open_days: [WeekDays.MONDAY, WeekDays.THURSDAY, WeekDays.SUNDAY]
+}
+
+const Diligence = {
+  ...TaishanMansion,
+  is_open_days: [WeekDays.TUESDAY, WeekDays.FRIDAY, WeekDays.SUNDAY]
+}
+
+const Gold = {
+  ...TaishanMansion,
+  is_open_days: [WeekDays.WEDNESDAY, WeekDays.SATURDAY, WeekDays.SUNDAY]
+}
+
 export const Dungeon: IDungeon = {
   mora: {
     name: 'ley_line_gold',
@@ -40,5 +62,14 @@ export const Dungeon: IDungeon = {
   philosophies_of_resistance: {...Resistance},
   teachings_of_freedom: {...Freedom},
   guide_to_freedom: {...Freedom},
-  philosophies_of_freedom: {...Freedom}
+  philosophies_of_freedom: {...Freedom},
+  teachings_of_prosperity: {...Prosperity},
+  guide_to_prosperity: {...Prosperity},
+  philosophies_of_prosperity: {...Prosperity},
+  teachings_of_diligence: {...Diligence},
+  guide_to_diligence: {...Diligence},
+  philosophies_of_diligence: {...Diligence},
+  teachings_of_gold: {...Gold},
+  guide_to_gold: {...Gold},
+  philosophies_of_gold: {...Gold},
 }
