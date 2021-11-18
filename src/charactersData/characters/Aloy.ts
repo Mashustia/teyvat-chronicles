572246a2b7f0a51d64a383346fbe836798445d6b
Mashustia <denisova.mariya.dm@gmail.com>
2021-11-19 00:29:57 +0300

    refactor(characters): refactor talent_materials for Albedo, Aloy and Amber

--- src/charactersData/characters/Aloy.ts
+++ src/charactersData/characters/Aloy.ts
@@ -1,46 +1,47 @@
 import {Vision} from '../../const/consts';
-import {Books, BossMaterials, BossSkillMaterial, CROWN_OF_INSIGHT, Gem, Materials, MORA, Rocks} from '../materialNames';
+import {Books, BossMaterials, BossSkillMaterial, Gem, Materials, MORA, Rocks} from '../materialNames';
+import {fillTalentMaterials} from '../../utils/utils';
 
 const Aloy = {
   name: 'Aloy',
   vision: Vision.CRYO,
   ascension_materials: {
     20: [
-      { material: Gem.CRYO.SHIVADA_JADE_SLIVER, count: 1 },
+      { material: Gem.CRYO.SHIVADA_JADE_SLIVER, count: 1, sorting_index: 1 },
       { material: Rocks.CRYSTAL_MARROW, count: 3 },
       { material: Materials.SPECTRAL_HUSK, count: 3 },
       { material: MORA, count: 20000 },
     ],
     40: [
-      { material: Gem.CRYO.SHIVADA_JADE_FRAGMENT, count: 3 },
+      { material: Gem.CRYO.SHIVADA_JADE_FRAGMENT, count: 3, sorting_index: 2 },
       { material: BossMaterials.CRYSTALLINE_BLOOM, count: 2 },
       { material: Rocks.CRYSTAL_MARROW, count: 10 },
       { material: Materials.SPECTRAL_HUSK, count: 15 },
       { material: MORA, count: 40000 },
     ],
     50: [
-      { material: Gem.CRYO.SHIVADA_JADE_FRAGMENT, count: 6 },
+      { material: Gem.CRYO.SHIVADA_JADE_FRAGMENT, count: 6, sorting_index: 2 },
       { material: BossMaterials.CRYSTALLINE_BLOOM, count: 4 },
       { material: Rocks.CRYSTAL_MARROW, count: 20 },
       { material: Materials.SPECTRAL_HEART, count: 12 },
       { material: MORA, count: 60000 },
     ],
     60: [
-      { material: Gem.CRYO.SHIVADA_JADE_CHUNK, count: 3 },
+      { material: Gem.CRYO.SHIVADA_JADE_CHUNK, count: 3, sorting_index: 3 },
       { material: BossMaterials.CRYSTALLINE_BLOOM, count: 8 },
       { material: Rocks.CRYSTAL_MARROW, count: 30 },
       { material: Materials.SPECTRAL_HEART, count: 18 },
       { material: MORA, count: 80000 },
     ],
     70: [
-      { material: Gem.CRYO.SHIVADA_JADE_CHUNK, count: 6 },
+      { material: Gem.CRYO.SHIVADA_JADE_CHUNK, count: 6, sorting_index: 3 },
       { material: BossMaterials.CRYSTALLINE_BLOOM, count: 12 },
       { material: Rocks.CRYSTAL_MARROW, count: 45 },
       { material: Materials.SPECTRAL_NUCLEUS, count: 12 },
       { material: MORA, count: 100000 },
     ],
     80: [
-      { material: Gem.CRYO.SHIVADA_JADE_GEMSTONE, count: 6 },
+      { material: Gem.CRYO.SHIVADA_JADE_GEMSTONE, count: 6, sorting_index: 4 },
       { material: BossMaterials.CRYSTALLINE_BLOOM, count: 20 },
       { material: Rocks.CRYSTAL_MARROW, count: 60 },
       { material: Materials.SPECTRAL_NUCLEUS, count: 24 },
@@ -48,58 +49,19 @@ const Aloy = {
     ]
   },
   rarity: 5,
-  talent_materials: {
-    2: [
-      {material: Books.TEACHINGS_OF_FREEDOM, count: 3},
-      {material: Materials.SPECTRAL_HUSK, count: 6},
-      {material: MORA, count: 12500}
-    ],
-    3: [
-      {material: Books.GUIDE_TO_FREEDOM, count: 2},
-      {material: Materials.SPECTRAL_HEART, count: 3},
-      {material: MORA, count: 17500}
-    ],
-    4: [
-      {material: Books.GUIDE_TO_FREEDOM, count: 4},
-      {material: Materials.SPECTRAL_HEART, count: 4},
-      {material: MORA, count: 25000}
-    ],
-    5: [
-      {material: Books.GUIDE_TO_FREEDOM, count: 6},
-      {material: Materials.SPECTRAL_HEART, count: 6},
-      {material: MORA, count: 30000}
-    ],
-    6: [
-      {material: Books.GUIDE_TO_FREEDOM, count: 9},
-      {material: Materials.SPECTRAL_HEART, count: 9},
-      {material: MORA, count: 37500}
-    ],
-    7: [
-      {material: Books.PHILOSOPHIES_OF_FREEDOM, count: 4},
-      {material: Materials.SPECTRAL_NUCLEUS, count: 4},
-      {material: BossSkillMaterial.MOLTEN_MOMENT, count: 1},
-      {material: MORA, count: 120000}
-    ],
-    8: [
-      {material: Books.PHILOSOPHIES_OF_FREEDOM, count: 6},
-      {material: Materials.SPECTRAL_NUCLEUS, count: 6},
-      {material: BossSkillMaterial.MOLTEN_MOMENT, count: 1},
-      {material: MORA, count: 260000}
-    ],
-    9: [
-      {material: Books.PHILOSOPHIES_OF_FREEDOM, count: 12},
-      {material: Materials.SPECTRAL_NUCLEUS, count: 9},
-      {material: BossSkillMaterial.MOLTEN_MOMENT, count: 2},
-      {material: MORA, count: 450000}
-    ],
-    10: [
-      {material: Books.PHILOSOPHIES_OF_FREEDOM, count: 16},
-      {material: Materials.SPECTRAL_NUCLEUS, count: 12},
-      {material: BossSkillMaterial.MOLTEN_MOMENT, count: 2},
-      {material: CROWN_OF_INSIGHT, count: 1},
-      {material: MORA, count: 700000}
-    ]
-  }
+  talent_materials: fillTalentMaterials({
+    books: {
+      1: Books.TEACHINGS_OF_FREEDOM,
+      2: Books.GUIDE_TO_FREEDOM,
+      3: Books.PHILOSOPHIES_OF_FREEDOM,
+    },
+    materials: {
+      1: Materials.SPECTRAL_HUSK,
+      2: Materials.SPECTRAL_HEART,
+      3: Materials.SPECTRAL_NUCLEUS,
+    },
+    bossMaterial: BossSkillMaterial.MOLTEN_MOMENT
+  })
 }
 
 export default Aloy
--- src/charactersData/characters/Amber.ts
+++ src/charactersData/characters/Amber.ts
@@ -3,112 +3,73 @@ import {
   Books,
   BossMaterials,
   BossSkillMaterial,
-  CROWN_OF_INSIGHT,
   Flower,
   Gem,
   Materials,
   MORA
 } from '../materialNames';
+import {fillTalentMaterials} from '../../utils/utils';
 
 const Amber = {
   name: 'Amber',
   vision: Vision.PYRO,
   ascension_materials: {
     20: [
-      { material: Gem.PYRO.AGNIDUS_AGATE_SLIVER, count: 1 },
-      { material: Flower.SMALL_LAMP_GRASS, count: 3 },
-      { material: Materials.FIRM_ARROWHEAD, count: 3 },
-      { material: MORA, count: 20000 },
+      {material: Gem.PYRO.AGNIDUS_AGATE_SLIVER, count: 1},
+      {material: Flower.SMALL_LAMP_GRASS, count: 3},
+      {material: Materials.FIRM_ARROWHEAD, count: 3},
+      {material: MORA, count: 20000},
     ],
     40: [
-      { material: Gem.PYRO.AGNIDUS_AGATE_FRAGMENT, count: 3 },
-      { material: BossMaterials.EVERFLAME_SEED, count: 2 },
-      { material: Flower.SMALL_LAMP_GRASS, count: 10 },
-      { material: Materials.FIRM_ARROWHEAD, count: 15 },
-      { material: MORA, count: 40000 },
+      {material: Gem.PYRO.AGNIDUS_AGATE_FRAGMENT, count: 3},
+      {material: BossMaterials.EVERFLAME_SEED, count: 2},
+      {material: Flower.SMALL_LAMP_GRASS, count: 10},
+      {material: Materials.FIRM_ARROWHEAD, count: 15},
+      {material: MORA, count: 40000},
     ],
     50: [
-      { material: Gem.PYRO.AGNIDUS_AGATE_FRAGMENT, count: 6 },
-      { material: BossMaterials.EVERFLAME_SEED, count: 4 },
-      { material: Flower.SMALL_LAMP_GRASS, count: 20 },
-      { material: Materials.SHARP_ARROWHEAD, count: 12 },
-      { material: MORA, count: 60000 },
+      {material: Gem.PYRO.AGNIDUS_AGATE_FRAGMENT, count: 6},
+      {material: BossMaterials.EVERFLAME_SEED, count: 4},
+      {material: Flower.SMALL_LAMP_GRASS, count: 20},
+      {material: Materials.SHARP_ARROWHEAD, count: 12},
+      {material: MORA, count: 60000},
     ],
     60: [
-      { material: Gem.PYRO.AGNIDUS_AGATE_CHUNK, count: 3 },
-      { material: BossMaterials.EVERFLAME_SEED, count: 8 },
-      { material: Flower.SMALL_LAMP_GRASS, count: 30 },
-      { material: Materials.SHARP_ARROWHEAD, count: 18 },
-      { material: MORA, count: 80000 },
+      {material: Gem.PYRO.AGNIDUS_AGATE_CHUNK, count: 3},
+      {material: BossMaterials.EVERFLAME_SEED, count: 8},
+      {material: Flower.SMALL_LAMP_GRASS, count: 30},
+      {material: Materials.SHARP_ARROWHEAD, count: 18},
+      {material: MORA, count: 80000},
     ],
     70: [
-      { material: Gem.PYRO.AGNIDUS_AGATE_CHUNK, count: 6 },
-      { material: BossMaterials.EVERFLAME_SEED, count: 12 },
-      { material: Flower.SMALL_LAMP_GRASS, count: 45 },
-      { material: Materials.WEATHERED_ARROWHEAD, count: 12 },
-      { material: MORA, count: 100000 },
+      {material: Gem.PYRO.AGNIDUS_AGATE_CHUNK, count: 6},
+      {material: BossMaterials.EVERFLAME_SEED, count: 12},
+      {material: Flower.SMALL_LAMP_GRASS, count: 45},
+      {material: Materials.WEATHERED_ARROWHEAD, count: 12},
+      {material: MORA, count: 100000},
     ],
     80: [
-      { material: Gem.PYRO.AGNIDUS_AGATE_GEMSTONE, count: 6 },
-      { material: BossMaterials.EVERFLAME_SEED, count: 20 },
-      { material: Flower.SMALL_LAMP_GRASS, count: 60 },
-      { material: Materials.WEATHERED_ARROWHEAD, count: 24 },
-      { material: MORA, count: 120000 },
+      {material: Gem.PYRO.AGNIDUS_AGATE_GEMSTONE, count: 6},
+      {material: BossMaterials.EVERFLAME_SEED, count: 20},
+      {material: Flower.SMALL_LAMP_GRASS, count: 60},
+      {material: Materials.WEATHERED_ARROWHEAD, count: 24},
+      {material: MORA, count: 120000},
     ]
   },
   rarity: 4,
-  talent_materials: {
-    2: [
-      {material: Books.TEACHINGS_OF_FREEDOM, count: 3},
-      {material: Materials.FIRM_ARROWHEAD, count: 6},
-      {material: MORA, count: 12500}
-    ],
-    3: [
-      {material: Books.GUIDE_TO_FREEDOM, count: 2},
-      {material: Materials.SHARP_ARROWHEAD, count: 3},
-      {material: MORA, count: 17500}
-    ],
-    4: [
-      {material: Books.GUIDE_TO_FREEDOM, count: 4},
-      {material: Materials.SHARP_ARROWHEAD, count: 4},
-      {material: MORA, count: 25000}
-    ],
-    5: [
-      {material: Books.GUIDE_TO_FREEDOM, count: 6},
-      {material: Materials.SHARP_ARROWHEAD, count: 6},
-      {material: MORA, count: 30000}
-    ],
-    6: [
-      {material: Books.GUIDE_TO_FREEDOM, count: 9},
-      {material: Materials.SHARP_ARROWHEAD, count: 9},
-      {material: MORA, count: 37500}
-    ],
-    7: [
-      {material: Books.PHILOSOPHIES_OF_FREEDOM, count: 4},
-      {material: Materials.WEATHERED_ARROWHEAD, count: 4},
-      {material: BossSkillMaterial.DVALINS_SIGH, count: 1},
-      {material: MORA, count: 120000}
-    ],
-    8: [
-      {material: Books.PHILOSOPHIES_OF_FREEDOM, count: 6},
-      {material: Materials.WEATHERED_ARROWHEAD, count: 6},
-      {material: BossSkillMaterial.DVALINS_SIGH, count: 1},
-      {material: MORA, count: 260000}
-    ],
-    9: [
-      {material: Books.PHILOSOPHIES_OF_FREEDOM, count: 12},
-      {material: Materials.WEATHERED_ARROWHEAD, count: 9},
-      {material: BossSkillMaterial.DVALINS_SIGH, count: 2},
-      {material: MORA, count: 450000}
-    ],
-    10: [
-      {material: Books.PHILOSOPHIES_OF_FREEDOM, count: 16},
-      {material: Materials.WEATHERED_ARROWHEAD, count: 12},
-      {material: BossSkillMaterial.DVALINS_SIGH, count: 2},
-      {material: CROWN_OF_INSIGHT, count: 1},
-      {material: MORA, count: 700000}
-    ]
-  }
+  talent_materials: fillTalentMaterials({
+    books: {
+      1: Books.TEACHINGS_OF_FREEDOM,
+      2: Books.GUIDE_TO_FREEDOM,
+      3: Books.PHILOSOPHIES_OF_FREEDOM,
+    },
+    materials: {
+      1: Materials.FIRM_ARROWHEAD,
+      2: Materials.SHARP_ARROWHEAD,
+      3: Materials.WEATHERED_ARROWHEAD,
+    },
+    bossMaterial: BossSkillMaterial.DVALINS_SIGH
+  })
 }
 
 export default Amber
