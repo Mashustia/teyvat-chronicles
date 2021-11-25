b94d703a5fbe97c89c30b07d72ae25b2eef247aa
Mashustia <denisova.mariya.dm@gmail.com>
2021-11-25 23:35:44 +0300

    feat(button): add component

--- src/common/Button/index.tsx
+++ src/common/Button/index.tsx
@@ -3,8 +3,11 @@ import {FC, ReactElement} from 'react'
 import './Button.css';
 import {IButtonProps as IProps} from './types';
 
-const Button: FC<IProps> = ({ children,  onClick }): ReactElement => (
-  <button className='button' onClick={onClick}>{children}</button>
+const Button: FC<IProps> = ({ children,  onClick, iconLeft }): ReactElement => (
+  <button className='button' onClick={onClick}>
+    {children}
+    {iconLeft}
+  </button>
 )
 
 export default Button
--- src/common/Button/types.ts
+++ src/common/Button/types.ts
@@ -1,3 +1,6 @@
+import {ReactElement} from 'react';
+
 export interface IButtonProps {
   onClick: () => void
+  iconLeft?: ReactElement
 }
