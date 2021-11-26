0783bef980bce02d596593dd43b660b6549d588c
Mashustia <denisova.mariya.dm@gmail.com>
2021-11-26 21:50:44 +0300

    feat(news): add twitter feed

--- src/views/News/index.tsx
+++ src/views/News/index.tsx
@@ -1,37 +1,27 @@
 import {FC, ReactElement} from 'react'
 import {Col, Container, Row} from 'react-bootstrap';
 import {useTranslation} from 'react-i18next';
+import {Timeline} from 'react-twitter-widgets'
 
 import {ScrollToTopOnMount} from '../../utils/utils';
-import {GenshinImpactTwitter, twitterUrl} from '../../const/consts';
+import {GenshinImpactTwitter} from '../../const/consts';
 
 const News: FC = (): ReactElement => {
   const {i18n, t} = useTranslation('common')
-  // const [genshinTwitterUrl, setTwitterURl] = useState(twitterUrl.replace('{{ twitterName }}', GenshinImpactTwitter[i18n.language]));
-  console.log(i18n.language)
-
-  // useEffect(() => {
-  //   return setTwitterURl(twitterUrl.replace('{{ twitterName }}', GenshinImpactTwitter[i18n.language]))
-  // }, [i18n])
-
-  const renderGenshinTwitter = (language: string): ReactElement => (
-    <a
-      className='twitter-timeline'
-      data-theme='dark'
-      href={twitterUrl.replace('{{ twitterName }}', GenshinImpactTwitter[language])}
-    >
-      {t('common:news')}
-    </a>
-  )
 
   return (
     <Container>
       <ScrollToTopOnMount/>
-
       <Row className='justify-content-center'>
         <Col sm={12} md={12} lg={8} xl={7} xxl={6}>
-          <h1 className='fs-3 mb-2'>{t('common:news', { twitter_name: GenshinImpactTwitter[i18n.language]} )}</h1>
-          {renderGenshinTwitter(i18n.language)}
+          <h1 className='fs-3 mb-2'>{t('common:news', {twitter_name: GenshinImpactTwitter[i18n.language]})}</h1>
+          <Timeline
+            dataSource={{
+              sourceType: 'profile',
+              screenName: GenshinImpactTwitter[i18n.language]
+            }}
+            options={{theme: 'dark'}}
+          />
         </Col>
       </Row>
     </Container>
