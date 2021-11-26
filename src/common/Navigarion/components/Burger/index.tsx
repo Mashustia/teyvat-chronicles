738ae805a2ea12a5c423e8f7c45438dbac366562
Mashustia <denisova.mariya.dm@gmail.com>
2021-11-26 15:12:19 +0300

    feat(burger): add menu button

---
+++ src/common/Navigarion/components/Burger/index.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react'
+
+import {ButtonType} from '../../../Button/types';
+import Button from '../../../Button';
+import {IBurgerProps as IProps} from './types';
+
+const Burger: FC<IProps> = ({ classes }) => {
+  const burgerImage = <img src='/images/icons/icon-menu.svg' alt='burger' width='22px' height='18px'/>
+
+  return <Button type={ButtonType.BUTTON} onClick={()=>{}} classes={classes}>{burgerImage}</Button>
+}
+
+export default Burger
