4264f34d10d3775d8b3f31578db88e215c100a0d
Mashustia <denisova.mariya.dm@gmail.com>
2021-11-29 23:35:11 +0300

    feat(common): add experience data

---
+++ src/charactersData/common.ts
@@ -0,0 +1,37 @@
+export const characterExperience = {
+  '1-20': {
+    exp_needed: 120175,
+    number_of_heros_wit: 7,
+    mora: 24035
+  },
+  '20-40': {
+    exp_needed: 578325,
+    number_of_heros_wit: 29,
+    mora: 115665
+  },
+  '40-50': {
+    exp_needed: 579100,
+    number_of_heros_wit: 29,
+    mora: 115820
+  },
+  '50-60': {
+    exp_needed: 854125,
+    number_of_heros_wit: 43,
+    mora: 170825
+  },
+  '60-70': {
+    exp_needed: 1195925,
+    number_of_heros_wit: 60,
+    mora: 239185
+  },
+  '70-80': {
+    exp_needed: 1611875,
+    number_of_heros_wit: 81,
+    mora: 322375
+  },
+  '80-90': {
+    exp_needed: 3423125,
+    number_of_heros_wit: 172,
+    mora: 684625
+  }
+}
