2ee6f378ef688f2f2c611caf42613e4f22b80a3a
Mashustia <denisova.mariya.dm@gmail.com>
2022-01-19 00:16:46 +0300

    feat(FarmingRoutes): rework routes position and behavior on page

--- src/views/Character/components/FarmigRoutes/index.tsx
+++ src/views/Character/components/FarmigRoutes/index.tsx
@@ -1,4 +1,4 @@
-import {FC} from 'react';
+import {FC, Fragment} from 'react';
 import {useTranslation} from 'react-i18next';
 
 import {Languages} from '../../../../const/consts';
@@ -18,21 +18,27 @@ const FarmingRoutes: FC<IFarmingRoutes> = ({route}) => {
   const routesSelectedByLanguage = getRoutes()
 
   const renderRouteLinks = () => {
-    return routesSelectedByLanguage?.map((link: string, index) => (
-      <div key={link}>
-        <a
-          href={link}
-          target='_blank'
-          rel='noreferrer'
-          className='link'
-        >{t('common:link_with_counter', {number: index+1})}</a>
-      </div>
-    ))
+    return routesSelectedByLanguage?.map((link: string, index) => {
+      const last = routesSelectedByLanguage?.length === index+1
+
+      return (
+        <Fragment key={link}>
+          <a
+            href={link}
+            target='_blank'
+            rel='noreferrer'
+            className='link'
+          >{t('common:link_with_counter', {number: index+1})}</a>
+
+          {!last && ', '}
+        </Fragment>
+      )
+    })
   }
 
   return routesSelectedByLanguage ? (
-    <div className='grid-2 grid-gap-x-lg mt-3'>
-      <p className='fw-bold mb-0 text-right'>{t('common:farming_routes')}:</p>
+    <div className='d-flex justify-content-center mt-2'>
+      <p className='fw-bold mb-0 me-2 flex-shrink-0'>{t('common:farming_routes')}:</p>
       <div className='text-left'>
         {renderRouteLinks()}
       </div>
