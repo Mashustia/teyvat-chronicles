f53a78d1d8984c06c68d8491411ab238d2fda463
Mashustia <denisova.mariya.dm@gmail.com>
2022-01-30 18:58:08 +0300

    style(CharacterInformation): delete row and add width style

--- src/views/Character/components/CharacterInformation/index.tsx
+++ src/views/Character/components/CharacterInformation/index.tsx
@@ -1,7 +1,6 @@
 import {FC, ReactElement} from 'react'
 import {RouteComponentProps, withRouter} from 'react-router-dom';
 import {useTranslation} from 'react-i18next';
-import {Row} from 'react-bootstrap';
 
 import {ICharacter} from '../../../../charactersData/types';
 import CHARACTERS from '../../../../charactersData';
@@ -9,9 +8,9 @@ import {IRouteParams} from '../../../../types/commonTypes';
 import CharacterImage from '../../../../common/CharacterImage';
 import Stars from '../../../../common/Stars';
 import LevelMaterials from '../LevelMaterials';
-import './CharacterInformation.css'
 import TalentMaterials from '../TalentMaterials';
 import PossibleTeams from '../PossibleTeams';
+import './CharacterInformation.css'
 
 const CharacterInformation: FC<RouteComponentProps<IRouteParams>> = ({match: {params}}): ReactElement => {
   const {t} = useTranslation(['character', 'common']);
@@ -24,19 +23,17 @@ const CharacterInformation: FC<RouteComponentProps<IRouteParams>> = ({match: {pa
 
   return (
     <div className='character-info'>
-      <Row className='justify-content-center gx-3 gy-3'>
-        <div className='grid'>
-          <CharacterImage name={name} withBorder={true}/>
-          <h1 className='fs-3 mb-0'>{t(`character:names.${name}`)}</h1>
-          <Stars count={rarity}/>
-        </div>
+      <div className='grid'>
+        <CharacterImage name={name} withBorder={true}/>
+        <h1 className='fs-3 mb-0'>{t(`character:names.${name}`)}</h1>
+        <Stars count={rarity}/>
+      </div>
 
-        <LevelMaterials/>
+      <LevelMaterials/>
 
-        <TalentMaterials/>
+      <TalentMaterials/>
 
-        <PossibleTeams/>
-      </Row>
+      <PossibleTeams/>
     </div>
   )
 }
