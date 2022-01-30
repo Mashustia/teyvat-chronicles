94b1a1f0d96c059f311023b2d5c4bc4f3fe591eb
Mashustia <denisova.mariya.dm@gmail.com>
2022-01-31 00:48:50 +0300

    feat(popover): add classes

--- src/common/Popover/index.tsx
+++ src/common/Popover/index.tsx
@@ -5,12 +5,12 @@ import {IPopoverProps as IProps, Position} from './types';
 import {useOnClickOutside} from '../../hooks/useOnClickOutside';
 import './Popover.css'
 
-const Popover: FC<IProps> = ({ children, isShown, onClickOutside, position }) => {
+const Popover: FC<IProps> = ({ children, isShown, onClickOutside, position, classes }) => {
   const popoverRef = useRef(null);
   useOnClickOutside(popoverRef, () => onClickOutside(!isShown))
 
   return isShown ? (
-    <div ref={popoverRef} className={cn('absolute', position === Position.RIGHT && 'popover--right')}>{children}</div>
+    <div ref={popoverRef} className={cn('absolute', position === Position.RIGHT && 'popover--right', classes)}>{children}</div>
   ) : null
 }
 
