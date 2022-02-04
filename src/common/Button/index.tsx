5f919166034c6232baf6dbdabe8be5f0750a8b35
Mashustia <denisova.mariya.dm@gmail.com>
2022-02-05 01:17:00 +0300

    feat(Button): add innerRef prop

--- src/common/Button/index.tsx
+++ src/common/Button/index.tsx
@@ -4,8 +4,8 @@ import cn from 'classnames';
 import './Button.css';
 import {ButtonType, IButtonProps as IProps} from './types';
 
-const Button: FC<IProps> = ({ children,  onClick, iconLeft, type, classes = ButtonType.BUTTON }): ReactElement => (
-  <button className={cn('button', classes)} onClick={onClick} type={type}>
+const Button: FC<IProps> = ({ children,  onClick, iconLeft, type, classes = ButtonType.BUTTON, innerRef }): ReactElement => (
+  <button className={cn('button', classes)} onClick={onClick} type={type} ref={innerRef}>
     {children}
     {iconLeft}
   </button>
