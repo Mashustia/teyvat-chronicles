8c13cdca45e69650f3de03e9992ce0701ca378b3
Mashustia <denisova.mariya.dm@gmail.com>
2022-02-04 21:11:36 +0300

    fix(languageSelector): fix toggle ref

--- src/common/LanguageSelector/index.tsx
+++ src/common/LanguageSelector/index.tsx
@@ -21,6 +21,7 @@ const LanguageSelector: FC<IDropdownProps> = ({languages, activeLanguage, onSele
   const renderOptions = () => (
     <div
       className={cn(isOpen ? 'open' : 'hidden', 'language-selector position-absolute d-flex flex-column py-2')}
+      ref={menuRef}
     >
       {languages.map((language: string) =>
         <span key={language} className='language-selector__option d-flex px-3 py-1' onClick={handleLanguageChange(language)}>
@@ -39,7 +40,7 @@ const LanguageSelector: FC<IDropdownProps> = ({languages, activeLanguage, onSele
   )
 
   return (
-    <div className='position-relative' ref={menuRef}>
+    <div className='position-relative'>
       <Button iconLeft={arrowDownIcon} onClick={handleDropdownToggle(!isOpen)}>{<Flags
         language={activeLanguage}/>}</Button>
       {isOpen && renderOptions()}
