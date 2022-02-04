2b63224e080afa5e00218100dcedb820e3aced12
Mashustia <denisova.mariya.dm@gmail.com>
2022-02-05 01:18:50 +0300

    fix(LevelSelect): fix double open for popover when click on level button

--- src/views/Character/components/Inputs/components/LevelSelect/index.tsx
+++ src/views/Character/components/Inputs/components/LevelSelect/index.tsx
@@ -1,4 +1,4 @@
-import {FC, ReactElement, useState} from 'react'
+import {FC, ReactElement, useState, MouseEvent, useRef, RefObject} from 'react'
 
 import Popover from '../../../../../../common/Popover';
 import Button from '../../../../../../common/Button';
@@ -7,8 +7,15 @@ import {ILevelSelect} from './types';
 import LevelSelectOptions from '../LevelSelectOptions';
 import './LevelSelect.css'
 
-const LevelSelect: FC<ILevelSelect> = ({ text, position, onSelectOption, levelInfo }): ReactElement => {
+const LevelSelect: FC<ILevelSelect> = ({text, position, onSelectOption, levelInfo}): ReactElement => {
   const [isPopoverShown, togglePopover] = useState(false)
+  const toggleButtonRef: RefObject<HTMLButtonElement> | null = useRef(null);
+
+  const handleOutsideClick = (e: MouseEvent | KeyboardEvent) => {
+    if (toggleButtonRef?.current?.contains(e.target as HTMLElement)) return
+
+    return togglePopover(!isPopoverShown)
+  }
 
   const handlePopoverToggle = () => togglePopover(!isPopoverShown)
 
@@ -21,6 +28,7 @@ const LevelSelect: FC<ILevelSelect> = ({ text, position, onSelectOption, levelIn
       <Button
         type={ButtonType.BUTTON}
         onClick={handlePopoverToggle}
+        innerRef={toggleButtonRef}
         classes='level-select__button'
       >
         <span className='d-flex align-items-center justify-content-center'>
@@ -28,7 +36,7 @@ const LevelSelect: FC<ILevelSelect> = ({ text, position, onSelectOption, levelIn
           {levelInfo.isAscended ? ascensionIcon : undefined}
         </span>
       </Button>
-      <Popover isShown={isPopoverShown} onClickOutside={togglePopover} position={position}>
+      <Popover isShown={isPopoverShown} onClickOutside={handleOutsideClick} position={position}>
         <LevelSelectOptions onSelectOption={onSelectOption} selectedLevel={levelInfo}/>
       </Popover>
     </div>
