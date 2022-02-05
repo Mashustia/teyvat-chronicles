7771ba75ce042007cce1f4314da882eecad03206
Mashustia <denisova.mariya.dm@gmail.com>
2022-02-06 01:31:28 +0300

    style(Popover): add z-index

--- src/common/Popover/index.tsx
+++ src/common/Popover/index.tsx
@@ -10,7 +10,7 @@ const Popover: FC<IProps> = ({ children, isShown, onClickOutside, position, clas
   useOnClickOutside(popoverRef, onClickOutside)
 
   return isShown ? (
-    <div ref={popoverRef} className={cn('absolute', position === Position.RIGHT && 'popover--right', classes)}>{children}</div>
+    <div ref={popoverRef} className={cn('absolute popover--index', position === Position.RIGHT && 'popover--right', classes)}>{children}</div>
   ) : null
 }
 
