ec83de6bf6ec745abe88d0e8a664ec3d1dcd7cdc
Mashustia <denisova.mariya.dm@gmail.com>
2022-02-09 00:03:32 +0300

    feat(SkillLevelGroup): add style file

--- src/views/Character/components/Inputs/components/SkillLevelGroup/index.tsx
+++ src/views/Character/components/Inputs/components/SkillLevelGroup/index.tsx
@@ -4,14 +4,15 @@ import {useTranslation} from 'react-i18next';
 import SkillLevelSelect from '../SkillLevelSelect';
 import {SKILL_LEVEL_OPTIONS} from '../../../../../../const/consts';
 import {Skill} from '../../../../../../const/translations';
+import './SkillLevelGroup.css'
 
 const SkillLevelGroup: FC = (): ReactElement => {
   const {t} = useTranslation(['character', 'common']);
 
   const renderGroup = (name: string) => {
     return (
-      <div className='grid-2 mb-2'>
-        <p className='mb-0 text-left'>{name}</p>
+      <div className='grid-2 my-2'>
+        <p className='mb-0 text-left fs-6'>{name}</p>
         <div className='grid-3'>
           <SkillLevelSelect id='attack_from' options={SKILL_LEVEL_OPTIONS}/>
           -
@@ -24,10 +25,10 @@ const SkillLevelGroup: FC = (): ReactElement => {
   const groups = Object.entries(Skill).map((groupName: [string, string]) => renderGroup(t(groupName[1])))
 
   return (
-    <div className='p-2'>
-      <h4 className='mb-3'>{t('character:talents_enhancement')}</h4>
+    <div>
+      <h4 className='mb-3 px-2'>{t('character:talents_enhancement')}</h4>
 
-      {groups}
+      <div className='skill-level-group table-border p-2'>{groups}</div>
     </div>
   )
 }
