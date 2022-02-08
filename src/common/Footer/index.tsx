95a2b73823ad9256c485e7572fea71098584f8f7
Mashustia <denisova.mariya.dm@gmail.com>
2022-02-08 21:57:30 +0300

    style(footer): add flex-wrap

--- src/common/Footer/index.tsx
+++ src/common/Footer/index.tsx
@@ -6,7 +6,7 @@ const Footer: FC = () => {
   const {t} = useTranslation('footer');
   return (
     <footer className='p-2'>
-      <div className='d-flex justify-content-center'>
+      <div className='d-flex justify-content-center flex-wrap'>
         <p className='d-block mb-0 me-1 mb-2'>{t('footer:contacts')}</p>
         <a href='mailto: marshmallowgoon21@gmail.com' className='link'>marshmallowgoon21@gmail.com</a>
       </div>
