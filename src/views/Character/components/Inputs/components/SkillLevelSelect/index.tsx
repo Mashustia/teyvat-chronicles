5b1c2024d57b464b391e9cabdadeefc4544728f7
Mashustia <denisova.mariya.dm@gmail.com>
2022-02-09 00:47:50 +0300

    feat(SkillLevelSelect): add style file

--- src/views/Character/components/Inputs/components/SkillLevelSelect/index.tsx
+++ src/views/Character/components/Inputs/components/SkillLevelSelect/index.tsx
@@ -1,14 +1,19 @@
 import {FC, ReactElement} from 'react'
 
 import {IOption, ISkillLevelSelectProps as IProps} from './types';
+import './SkillLevelSelect.css'
 
 const SkillLevelSelect: FC<IProps> = ({ id, name, options}): ReactElement => {
-  const selectOptions = options.map((option: IOption) => <option key={option.id}>{option.text}</option>)
+  const selectOptions = options.map((option: IOption) => (
+    <option key={option.id} className='skill-select__option'>
+      {option.text}
+    </option>
+  ))
 
   return (
     <div>
       {name && <label htmlFor={id} className='me-2'>{name}</label>}
-      <select id={id}>
+      <select id={id} className='skill-select__select ps-1'>
         {selectOptions}
       </select>
     </div>
