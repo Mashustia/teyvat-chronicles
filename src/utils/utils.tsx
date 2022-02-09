acada76a5027c977ebcf489c08a97089f27a037d
Mashustia <denisova.mariya.dm@gmail.com>
2022-02-09 22:16:34 +0300

    refactor(AscensionSummary): move all material calculations in functions

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -15,6 +15,7 @@ import {characterExperience, HEROS_WIT_EXP as HEROS_WIT_EXP_QUANTITY} from '../c
 import CHARACTERS from '../charactersData';
 import {COUNT, MATERIAL, SORTING_INDEX} from '../const/consts';
 import {HEROS_WIT, MORA} from '../charactersData/materials/materialNames';
+import {IAscensionSummary} from '../views/Character/components/AscensionSummary/types';
 
 export const getOrgDataFromMatch = (matchData: match<IRouteParams>): string => {
   if (matchData?.params?.name) {
@@ -203,4 +204,52 @@ export const calculateMaterials = (characterName: string, startingLevel: ILevel,
   }
 
   return []
+}
+
+export const getCumulativeMaterials = ({ characterData, skillLevel, isTraveler}: IAscensionSummary): IMaterial[] => {
+  const skills = Object.keys(skillLevel)
+
+  const cumulativeMaterials: IMaterial[] = []
+
+  skills.forEach((skill: string) => {
+    const level = skillLevel[skill]
+
+    let talentMaterials: IAscensionMaterials = {}
+
+    if (isTraveler) talentMaterials = characterData[skill]
+
+    if (!isTraveler && characterData?.talent_materials) talentMaterials = characterData.talent_materials
+
+    return Object.keys(talentMaterials).forEach((skillLevel: string) => {
+      if (skillLevel > level.from && skillLevel <= level.to) {
+        return cumulativeMaterials.push(...talentMaterials[skillLevel])
+      }
+      return
+    })
+  })
+
+  return cumulativeMaterials
+}
+
+export const getGroupedMaterials = (materials: IMaterial[]): IMaterial[] => {
+  const materialGroupedByName = groupBy(materials, MATERIAL)
+  const groupedMaterialKeys = Object.keys(materialGroupedByName)
+
+  return groupedMaterialKeys.reduce((accumulator: IMaterial[], currentValue: string) => {
+    const counter: number = materialGroupedByName[currentValue].reduce((materialAccumulator: number, materialCurrentValue: IMaterial) => {
+      return materialAccumulator + materialCurrentValue.count
+    }, 0)
+
+    const material: IMaterial = {
+      material: currentValue,
+      count: counter,
+    }
+
+    const sortingIndex = materialGroupedByName[currentValue][0]?.sorting_index
+    if (sortingIndex) {
+      material[SORTING_INDEX] = sortingIndex
+    }
+
+    return [...accumulator, material]
+  }, [])
 }
--- src/views/Character/components/AscensionSummary/index.tsx
+++ src/views/Character/components/AscensionSummary/index.tsx
@@ -1,56 +1,16 @@
 import {FC, ReactElement} from 'react'
-import { groupBy, sortBy } from 'lodash';
+import { sortBy } from 'lodash';
 
 import {IAscensionSummary} from './types';
-import {IAscensionMaterials, IMaterial} from '../../../../charactersData/types';
+import {IMaterial} from '../../../../charactersData/types';
 import Material from '../Material/Material';
+import {getCumulativeMaterials, getGroupedMaterials} from '../../../../utils/utils';
+import {COUNT, SORTING_INDEX} from '../../../../const/consts';
 
-const MATERIAL = 'material'
-const SORTING_INDEX = 'sorting_index'
-const COUNT = 'count'
+const AscensionSummary: FC<IAscensionSummary> = (props): ReactElement => {
+  const cumulativeMaterials: IMaterial[] = getCumulativeMaterials(props)
 
-const AscensionSummary: FC<IAscensionSummary> = ({ characterData, skillLevel, isTraveler}): ReactElement => {
-  const skills = Object.keys(skillLevel)
-
-  const cumulativeMaterials: IMaterial[] = []
-
-  skills.forEach((skill: string) => {
-    const level = skillLevel[skill]
-
-    let talentMaterials: IAscensionMaterials = {}
-
-    if (isTraveler) talentMaterials = characterData[skill]
-
-    if (!isTraveler && characterData?.talent_materials) talentMaterials = characterData.talent_materials
-
-    return Object.keys(talentMaterials).forEach((skillLevel: string) => {
-      if (skillLevel > level.from && skillLevel <= level.to) {
-        return cumulativeMaterials.push(...talentMaterials[skillLevel])
-      }
-      return
-    })
-  })
-
-  const materialGroupedByName = groupBy(cumulativeMaterials, MATERIAL)
-  const groupedMaterialKeys = Object.keys(materialGroupedByName)
-
-  const materials: IMaterial[] = groupedMaterialKeys.reduce((accumulator: IMaterial[], currentValue: string) => {
-    const counter: number = materialGroupedByName[currentValue].reduce((materialAccumulator: number, materialCurrentValue: IMaterial) => {
-      return materialAccumulator + materialCurrentValue.count
-    }, 0)
-
-    const material: IMaterial = {
-      material: currentValue,
-      count: counter,
-    }
-
-    const sortingIndex = materialGroupedByName[currentValue][0]?.sorting_index
-    if (sortingIndex) {
-      material[SORTING_INDEX] = sortingIndex
-    }
-
-    return [...accumulator, material]
-  }, [])
+  const materials: IMaterial[] = getGroupedMaterials(cumulativeMaterials)
 
   const sortedMaterials = sortBy(materials, [SORTING_INDEX, COUNT])
 
