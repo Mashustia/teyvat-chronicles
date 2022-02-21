4cc06274d294f0b6a073704c466646dd01b545bf
Mashustia <denisova.mariya.dm@gmail.com>
2022-02-21 11:58:24 +0300

    style(character): add bottom margin

--- src/views/Characters/components/Character/index.tsx
+++ src/views/Characters/components/Character/index.tsx
@@ -6,7 +6,7 @@ import {ICharacterProps as IProps} from './types';
 import './Character.css'
 
 const Character: FC<IProps> = ({name, rarity, translated_name}) => (
-  <div className='d-flex flex-column character mx-3'>
+  <div className='d-flex flex-column character mx-3 mb-4'>
     <CharacterImage name={name} withBorder={true} withLink={true}/>
     <h4 className='d-flex justify-content-center my-auto'>{translated_name}</h4>
     <div>{<Stars count={rarity}/>}</div>
