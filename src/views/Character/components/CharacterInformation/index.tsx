bca5e7680ab3dbd1887f5970400681966947a8e5
Mashustia <denisova.mariya.dm@gmail.com>
2022-02-25 23:44:46 +0300

    refactor(CharacterInformation): add useParams

--- src/views/Character/components/CharacterInformation/index.tsx
+++ src/views/Character/components/CharacterInformation/index.tsx
@@ -1,5 +1,5 @@
 import {FC, ReactElement} from 'react'
-import {RouteComponentProps, withRouter} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
 import {useTranslation} from 'react-i18next';
 
 import {ICharacter} from '../../../../charactersData/types';
@@ -12,20 +12,20 @@ import TalentMaterials from '../TalentMaterials';
 import PossibleTeams from '../PossibleTeams';
 import './CharacterInformation.css'
 
-const CharacterInformation: FC<RouteComponentProps<IRouteParams>> = ({match: {params}}): ReactElement => {
+const CharacterInformation: FC = (): ReactElement => {
   const {t} = useTranslation(['character', 'common']);
-  const {name} = params
-  const activeCharacter = CHARACTERS.find((character: ICharacter) => character.name === params.name)
+  const { name } = useParams<IRouteParams>();
+  const activeCharacter = CHARACTERS.find((character: ICharacter) => character.name === name)
 
   if (!activeCharacter) return <></>
 
-  const {rarity} = activeCharacter
+  const {rarity, name: characterName} = activeCharacter
 
   return (
     <div className='character-info'>
       <div className='grid mb-2'>
-        <CharacterImage name={name} withBorder={true}/>
-        <h1 className='fs-3 mb-0'>{t(`character:names.${name}`)}</h1>
+        <CharacterImage name={characterName} withBorder={true}/>
+        <h1 className='fs-3 mb-0'>{t(`character:names.${characterName}`)}</h1>
         <Stars count={rarity}/>
       </div>
 
@@ -38,4 +38,4 @@ const CharacterInformation: FC<RouteComponentProps<IRouteParams>> = ({match: {pa
   )
 }
 
-export default withRouter(CharacterInformation)
+export default CharacterInformation
