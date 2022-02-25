fad2bd5c7d03484b709a401561bf382fb93fb881
Mashustia <denisova.mariya.dm@gmail.com>
2022-02-25 23:40:46 +0300

    refactor(app): add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import {FC, Suspense} from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import './App.css';
 import Main from './views/Main';
 import Footer from './common/Footer';
 import Header from './common/Header';
+import {RouteName} from './const/consts';
 
 const App: FC = () => {
   return (
     <div className='app pt-2'>
       <Suspense fallback='Loading...'>
         <BrowserRouter>
-          <Route exact={true} component={Header} />
-          <Route exact={true} component={Main} />
-          <Route exact={true} component={Footer} />
+          <Routes>
+            <Route path={RouteName.ALL} element={<Header/>}/>
+          </Routes>
+          <Routes>
+            <Route path={RouteName.ALL} element={<Main/>}/>
+          </Routes>
+          <Routes>
+            <Route path={RouteName.ALL} element={<Footer/>}/>
+          </Routes>
         </BrowserRouter>
       </Suspense>
     </div>
