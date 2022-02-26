847eebef8fc44b1034f766062f7603a7ca484677
Mashustia <denisova.mariya.dm@gmail.com>
2022-02-27 00:25:36 +0300

    style(Page404): add styles

--- src/common/Page404/index.tsx
+++ src/common/Page404/index.tsx
@@ -1,15 +1,13 @@
 import {FC, ReactElement} from 'react'
 import {useTranslation} from 'react-i18next';
 
+import './Page404.css'
 
 const Page404: FC = (): ReactElement => {
   const {t} = useTranslation(['common'])
 
-
   return (
-    <div>
-      <h1>{t('common:page_404')}</h1>
-    </div>
+    <h1 className='fs-3 mb-0 absolute page-404'>{t('common:page_404')}</h1>
   )
 }
 
