c5b304fc7ca9ec994893054e4059c120c7b5bcf6
Mashustia <denisova.mariya.dm@gmail.com>
2022-03-30 19:11:36 +0300

    feat(KamisatoAyato): add Kamisato Ayato info

---
+++ src/charactersData/characters/KamisatoAyato.ts
@@ -0,0 +1,46 @@
+import {Character, Vision} from '../../const/consts';
+import {
+  BossMaterials,
+  Gem,
+  Materials,
+  Flower,
+  Books,
+  BossSkillMaterial,
+} from '../materials/materialNames';
+import {fillAscensionMaterials, fillTalentMaterials} from '../../utils/utils';
+
+const KamisatoAyato = {
+  name: Character.KAMISATO_AYATO,
+  vision: Vision.CRYO,
+  ascension_materials: fillAscensionMaterials({
+    gems: {
+      1: Gem.HYDRO.VARUNADA_LAZURITE_SLIVER,
+      2: Gem.HYDRO.VARUNADA_LAZURITE_FRAGMENT,
+      3: Gem.HYDRO.VARUNADA_LAZURITE_CHUNK,
+      4: Gem.HYDRO.VARUNADA_LAZURITE_GEMSTONE
+    },
+    materials: {
+      1: Materials.OLD_HANDGUARD,
+      2: Materials.KAGEUCHI_HANDGUARD,
+      3: Materials.FAMED_HANDGUARD,
+    },
+    bossMaterial: BossMaterials.DEW_OF_REPUDIATION,
+    specialty: Flower.SAKURA_BLOOM
+  }),
+  rarity: 5,
+  talent_materials: fillTalentMaterials({
+    books: {
+      1: Books.TEACHINGS_OF_ELEGANCE,
+      2: Books.GUIDE_TO_ELEGANCE,
+      3: Books.PHILOSOPHIES_OF_ELEGANCE,
+    },
+    materials: {
+      1: Materials.OLD_HANDGUARD,
+      2: Materials.KAGEUCHI_HANDGUARD,
+      3: Materials.FAMED_HANDGUARD,
+    },
+    bossMaterial: BossSkillMaterial.MUDRA_OF_THE_MALEFIC_GENERAL
+  })
+}
+
+export default KamisatoAyato
