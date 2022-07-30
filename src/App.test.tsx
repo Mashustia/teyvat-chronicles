f5d0ef0a221a839c6e9706f0ebfd9b5df39286ff
mariia_denisova <denisova.m.dm@gmail.com>
2022-07-31 00:34:41 +0600

    fix(app.test): changed test for App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,18 @@
-import { render } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
+import {I18nextProvider} from 'react-i18next';
+import i18n from './i18nTestingConfig';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('App', () => {
+  test('renders App component', async () => {
+    render(
+      <I18nextProvider i18n={i18n}>
+        <App/>
+      </I18nextProvider>
+    )
+
+    expect(screen.getByTestId('app')).toBeInTheDocument()
+  })
 });
+
