8fd6fcbef807d84160d4d56f5b93d62b68e37dbe
mariia_denisova <denisova.m.dm@gmail.com>
2023-01-28 18:17:32 +0600

    feat(footer): added github link

--- src/common/Footer/index.tsx
+++ src/common/Footer/index.tsx
@@ -4,11 +4,17 @@ import {useTranslation} from 'react-i18next';
 
 const Footer: FC = () => {
   const {t} = useTranslation('footer');
+
   return (
     <footer className='p-2'>
       <div className='d-flex justify-content-center flex-wrap'>
         <p className='d-block mb-0 me-1 mb-2'>{t('footer:contacts')}</p>
         <a href='mailto: teyvatchroniclesapp@gmail.com' className='link'>teyvatchroniclesapp@gmail.com</a>
+      </div>
+      <div className='d-flex justify-content-center flex-wrap'>
+        <a href='https://github.com/Mashustia/teyvat-chronicles' className='link' target='_blank' rel='noreferrer'>
+          {t('footer:github')}
+        </a>
       </div>
       <div className='d-flex justify-content-center flex-column'>
         <p className='d-block mb-0'>{t('footer:trademark_1')}</p>
